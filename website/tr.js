f4cf1655d092768e8cf8ead833fabfeecbcbc13c
RaMMicHaeL <leahcimmar@gmail.com>
2013-12-02 21:37:15 +0200

    Turkish translation of the website

---
+++ website/tr.js
@@ -0,0 +1,122 @@
+jquery_lang_js.prototype.lang.tr = {
+	
+	// Header
+	
+	'Home':
+		'Anasayfa',
+	'Press':
+		'Basın',
+	'FAQ':
+		'SSS',
+	'Blog':
+		'Blog',
+	'Donate':
+		'Bağış yap',
+	'Contact':
+		'İletişim',
+	'Language':
+		'Dil',
+	
+	// Footer
+	
+	'Copyright © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Telif hakkı © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>.',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Yazılım yüklerken alakasız tekliflerin işaretini kaldırmaktan bıktınız mı?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Hiç yazılım yüklerken, yükleyicinin her ne pahasına olursa olsun istenmeyen programları yüklemeye çalıştığını hissettiniz mi? Peki, bir onay kutusunu kaçırıp, sonrasında saatlerce reklam yazılımları kaldırmaya çalıştınız mı? Yazılımı yükledikten sonra, yeni bir ana sayfa, yeni bir arama motoru, hatta yeni bir tarayıcınız olduğunu gördünüz mü?<br>Unchecky, bu istenmeyen programları bilgisayarınızdan uzak tutmayı amaçlamaktadır.',
+	'Download':
+		'İNDİR',
+	
+	'Introducing Unchecky!':
+		'Unchecky Tanıtımı!',
+	'Keeps your checkboxes clear.':
+		'Onay kutularınızı temiz tutar.',
+	
+	'Unchecks':
+		'İşaretleri kaldırır',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky otomatik olarak alakasız tekliflerin işaretini kaldırır , hem daha az fare tıklaması yapmanızı hem de istenmeyen bir uygulamanın onay kutusunu kaçırmamanızı sağlar.',
+	
+	'Warns':
+		'Uyarır',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Yükleyiciler genellikle kurulumun doğal bir parçası gibi ek programları gizlice yüklemeyi dener. Unchecky istenmeyebilecek türden bir teklifi kabul etmeye çalıştığınızda sizi uyarır, yanlışlıkla kabul edilmesi olasılığını azaltır.',
+	
+	'Updates automatically':
+		'Otomatik güncelleştirilir',
+	'Yükleyin ve unutun. Yeni bir sürüm mevcut olduğunda Unchecky güncellemeleri otomatik olarak yükler, böylece en son sürümü çalıştırıp çalıştırmadığınızdan endişelenmenize gerek kalmaz.':
+		'',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'İnsanların Unchecky hakkında söyledikleri',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky ayrıca aşağıdaki uluslararası web sitelerinde yer aldı',
+	'on':
+		'',
+	
+	'People are showing Unchecky in action':
+		'İnsanların Unchecky tanıtımları',
+	
+	'Portuguese':
+		'Portekizce',
+	'Russian':
+		'Rusça',
+	'Spanish':
+		'İspanyolca',
+	'Italian':
+		'İtalyanca',
+	'French':
+		'Fransızca',
+	'French':
+		'Fransızca',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Unchecky için sistem gereksinimleri nelerdir?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky Windows XP ve yukarısını desteklemektedir.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Bir hatayı bildirmek veya bir özellik önermek için en iyi yol nedir?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'<a href="http://unchecky.userecho.com/">UserEcho sayfası</a> üzerinde görüşlerinizi bildirebilirsiniz.',
+	
+	'I want to translate Unchecky.':
+		'Unchecky\'yi çevirmek istiyorum',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Unchecky programının ve web sitesinin çeviri dosyaları <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository üzerinde bulunabilir.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Hataları bildirmek veya özellik önermek için lütfen bu iletişim formunu kullanmayın. Geribildirimleriniz için <a href="http://unchecky.userecho.com/">UserEcho sayfası</a>nı kullanın.',
+	
+	'First Name':
+		'Ad',
+	'Last Name':
+		'Soyad',
+	'Email Address':
+		'E-posta adresi',
+	'Subject':
+		'Konu',
+	'Message':
+		'Mesaj',
+	'Send':
+		'Gönder',
+	
+	// 404
+	
+	'Not found':
+		'Bulunamadı',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Üzgünüz ama aradığınız sayfa bulunamıyor',
+	
+}
