92d4810472b9ead142dada687026398191c97876
Torsten Augustsson <t.dotar@gmail.com>
2013-12-05 20:41:51 +0100

    Swedish translation
    All files translated to Swedish

---
+++ website/se.js
@@ -0,0 +1,125 @@
+jquery_lang_js.prototype.lang.fr = {
+	
+	// Header
+	
+	'Home':
+		'Hem',
+	'Press':
+		'Press',
+	'FAQ':
+		'FAQ',
+	'Blog':
+		'Blogg',
+	'Donate':
+		'Donera',
+	'Contact':
+		'Kontakt',
+	'Language':
+		'Språk',
+	
+	// Footer
+	
+	'Copyright © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Copyright © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>. Svensk översättning: <a href="mailto:t.dotar@gmail.com?Subject=Unchecky">Torsten Augustsson</a>',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Trött på att välja bort orelaterade erbjudanden när du installerar mjukvara?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Har du någonsin känt, när du installerar mjukvara, att du blivit lurad att installera oönskade program? Har du någonsin missat en kryssruta och därefter spenderat timmar på att ta bort reklamprogram? Har du någonsin öppnat din webbläsare efter en installation och upptäckt att du har en ny startsida, en ny sökmotor eller till och med en ny webbläsare?<br>Unchecky försöker hålla oönskade program borta från din dator.',
+	'Download':
+		'Ladda ner',
+	
+	'Introducing Unchecky!':
+		'Härmed presenteras Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Tömmer dina kryssrutor.',
+	
+	'Unchecks':
+		'Kryssar ur',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky kryssar automatiskt ur orelaterade erbjudanden. Det sparar både på musklick och gör det mindre sannolikt att du skulle missa en kryssruta eller ett oönskat erbjudande.',
+	
+	'Warns':
+		'Varnar',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Många gånger maskeras extra program som en del av installationen. Unchecky varnar dig när du försöker acceptera ett potentiellt oönskat erbjudande, vilket gör det mindre sannolikt att accepteras av misstag.',
+	
+	'Updates automatically':
+		'Automatiska uppdateringar',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Installera och förglöm. Unchecky uppdateras automatiskt när en ny version är tillgänglig, så du slipper oroa dig för om du kör den senaste versionen.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Det snackas om Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky var också med på följande internationella webbplatser:',
+	'on':
+		'från',
+	
+	'People are showing Unchecky in action':
+		'Folk visar Unchecky i praktiken:',
+	
+	'Portuguese':
+		'portugisiska',
+	'Russian':
+		'ryska',
+	'Spanish':
+		'spanska',
+	'Italian':
+		'italienska',
+	'French':
+		'franska',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Vad har Unchecky för systemkrav?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky stöder Windows XP och senare.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Mitt antivirusprogram flaggar Unchecky som skadligt',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
+		'Det är en falsk positiv. Var vänlig rapportera om det till ditt antivirusföretag. Notera att Unchekcy genomgående testats av Softpedia och <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">konstaterades vara 100% rent</a>',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Hur rapporterar jag enklast buggar och föreslår funktioner?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Skicka in din feedback på vår sida på <a href="http://unchecky.userecho.com/">UserEcho</a>',
+	
+	'I want to translate Unchecky.':
+		'Jag vill översätta Unchecky!',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Översättningsfilerna för Uncheckys mjukvara och webbsida kan hittas på Github: <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Använd inte detta kontaktformulär för att rapportera buggar eller föreslå funktioner. Skicka in din feedback till vår sida på <a href="http://unchecky.userecho.com/">UserEcho</a>.',
+	
+	'First Name':
+		'Förnamn',
+	'Last Name':
+		'Efternamn',
+	'Email Address':
+		'E-port',
+	'Subject':
+		'Ämne',
+	'Message':
+		'Meddelande',
+	'Send':
+		'Skicka',
+	
+	// 404
+	
+	'Not found':
+		'Sidan hittades inte',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Sidan du letade efter kunde inte hittas.',
+	
+}
