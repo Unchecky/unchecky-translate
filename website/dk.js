5ff39b39664c24cf8454f40ebfcfd91ec96f28ea
fbjswf <fbjohansen@msn.com>
2013-12-17 20:45:44 +0100

    dk.js
    Trying again :-)

---
+++ website/dk.js
@@ -0,0 +1,145 @@
+jquery_lang_js.prototype.lang.dk = {
+        
+        // Header
+        
+        'Home':
+                'Hjem',
+        'Press':
+                'Presse',
+        'FAQ':
+                'FAQ',
+        'Changelog':
+                '',
+        'Contact':
+                'Kontakt',
+        'Links':
+                '',
+        'Bug/feature tracker':
+                '',
+        'Blog':
+                'Blog',
+        'Donate':
+                'Doner',
+        'Language':
+                'Sprog',
+        
+        // Footer
+        
+        'Copyright © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+                'Copyright © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>. Dansk oversættelse: <a href="mailto:fbjohansen@gmail.com?Subject=Unchecky">Finn Johansen</a>',
+        
+        // Home
+        
+        'Tired of unchecking unrelated offers while installing software?':
+                'Træt af at skulle fravælge uønskede tilbud, når du installerer programmer?',
+        'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+                'Har du nogensinde oplevet, at mens du installerer et program forsøger installationen at lægge ekstra uønskede programmer ind på din computer? Har du nogensinde overset en checkboks og efterfølgende brugt timer på at fjerne adware? Har du nogensinde åbnet din browser efter en installation og fundet, at du har fået en ny startside, en ny søgemaskine eller endda en ny browser?<br>Unchecky forsøger at holde potentielt uønskede programmer væk fra din computer.',
+        'Download':
+                'Hent',
+        
+        'Introducing Unchecky!':
+                'Hermed introduceres Unchecky!',
+        'Keeps your checkboxes clear.':
+                'Tømmer dine checkbokse.',
+        
+        'Unchecks':
+                'Fjerner checkmærker',
+        'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+                'Unchecky fjerner automatisk checkmærker (flueben) ved uønskede tilbud og sparer dig for ekstra museklik og gør det mindre sandsynligt, at du skulle overse en checkboks eller et uønsket tilbud.',
+        
+        'Warns':
+                'Advarer',
+        'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+                'Ofte forsøger installationer at maskere ekstra programmer som en del af installationen. Unchecky advarer dig når du forsøger at acceptere et potentielt uønsket tilbud. Det gør det mindre sandsynligt, at du accepterer et uønsket tilbud ved en fejltagelse.',
+        
+        'Updates automatically':
+                'Opdateres automatisk',
+        'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+                'Installer og glem. Unchecky opdateres automatiskt når en ny version er tilgængelig, så du slipper for at bekymre dig om du har den nyeste version.',
+        
+        // Press
+        
+        'People are talking about Unchecky':
+                'Man taler om Unchecky',
+        'Unchecky was also featured on the following international websites:':
+                'Unchecky var også med på følgende internationale websteder:',
+        'on':
+                'fra',
+        'Awards':
+                '',
+        
+        'People are showing Unchecky in action':
+                'Folk viser Unchecky i praksis:',
+        'Demonstrating Unchecky':
+                'Demonstrerer Unchecky',
+        
+        'Portuguese':
+                'portugisisk',
+        'Polish':
+                'polsk',
+        'Russian':
+                'russisk',
+        'Spanish':
+                'spansk',
+        'Italian':
+                'italiensk',
+        'French':
+                'fransk',
+        
+        // FAQ
+        
+        'What are the system requirements for Unchecky?':
+                'Hvad er systemkravene for at kunne køre Unchecky?',
+        'Unchecky supports Windows XP and above.':
+                'Unchecky kan køre på Windows XP og senere versioner.',
+        
+        'My antivirus flags Unchecky as malicious.':
+                'Mit antivirusprogram siger at Unchecky er skadeligt',
+        'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
+                'Det er en falsk positiv. Venligst rapporter dette til producenten af dit antivirusprogram. Bemærk at Unchekcy er grundigt testet af Softpedia og <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">blev konstateret 100% rent</a>',
+        
+        'What is the best way to report a bug or suggest a feature?':
+                'Hvordan rapporterer jeg nemmest fejl eller foreslår nye funktioner?',
+        'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+                'Send din feedback på vores side via dette link<a href="http://unchecky.userecho.com/">UserEcho</a>',
+        
+        'I want to translate Unchecky.':
+                'Jeg vil oversætte Unchecky!',
+        'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+                'Oversættelsesfiler til Uncheckys software og webside kan findes på Github: <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>',
+        
+        // Contact
+        
+        'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+                'Anvend venligst ikke denne kontaktformular til at rapportere fejl eller til at foreslå nye funktioner. Send din feedback via vores webside <a href="http://unchecky.userecho.com/">UserEcho</a>.',
+        
+        'First Name':
+                'Fornavn',
+        'Last Name':
+                'Efternavn',
+        'Email Address':
+                'E-mail adresse',
+        'Subject':
+                'Emne',
+        'Message':
+                'Meddelelse',
+        'Send':
+                'Send',
+        'Please fill in all fields.':
+				'Venligst udfyld alle felter',
+		'Please enter a valid email address.':
+				'Venligst udfyld med gyldig e-mail adresse',
+		'Could not complete your request.':
+				'Kunne ikke færdiggøre din anmodning',
+		'The message has been sent successfully.':
+				'Beskeden er blevet sendt',
+        
+        // 404
+        
+        'Not found':
+                'Siden blev ikke fundet',
+        
+        'Sorry but the page you are looking for cannot be found.':
+                'Siden du ledte efter kunne ikke findes.',
+        
+}
