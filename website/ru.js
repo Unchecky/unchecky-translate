db4202d9b9142707e2f4509aa482ea4fe0a7f442
RaMMicHaeL <leahcimmar@gmail.com>
2013-12-18 19:41:54 +0200

    Website, Russian: a couple of improvements

--- website/ru.js
+++ website/ru.js
@@ -9,13 +9,13 @@ jquery_lang_js.prototype.lang.ru = {
 	'FAQ':
 		'ЧАВО',
 	'Changelog':
-		'',
+		'Список изменений',
 	'Contact':
 		'Обратная связь',
 	'Links':
-		'',
+		'Ссылки',
 	'Bug/feature tracker':
-		'',
+		'Ошибки и пожелания',
 	'Blog':
 		'Блог',
 	'Donate':
@@ -26,14 +26,14 @@ jquery_lang_js.prototype.lang.ru = {
 	// Footer
 	
 	'Copyright © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
-		'',
+		'Copyright © 2013 <a href="http://rammichael.com/">RaMMicHaeL</a>. Перевод: Усманов Алешер.',
 	
 	// Home
 	
 	'Tired of unchecking unrelated offers while installing software?':
-		'Надоело снимать галочки с нежелательного ПО во время установки программ?',
+		'Надоело снимать галочки с нежелательными предложениями во время установки программ?',
 	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
-		'Вы когда-нибудь чувствовали, что при установке ПО установщик пытается заставить вас любой ценой установить дополнительно нежелательные программы? Когда-нибудь пропускали галочку и потом часами удаляли нежелательное ПО? Когда-нибудь открывали свой браузер, только чтобы узнать, что у вас новая домашняя страница, новая поисковая система, или даже новый браузер? Unchecky поможет оградить ваш компьютер от потенциально нежелательных программ.',
+		'Вы когда-нибудь чувствовали, что при установке программ установщик пытается заставить вас любой ценой установить дополнительные нежелательные программы? Когда-нибудь пропускали галочку и потом часами удаляли нежелательное программное обеспечение? Когда-нибудь открывали свой браузер, только чтобы узнать, что у вас новая домашняя страница, новая поисковая система, или даже новый браузер?<br>Unchecky поможет оградить ваш компьютер от потенциально нежелательных программ.',
 	'Download':
 		'Скачать',
 	
@@ -45,38 +45,38 @@ jquery_lang_js.prototype.lang.ru = {
 	'Unchecks':
 		'Снимает галочки за вас',
 	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
-		'Unchecky автоматически снимает галочки дополнительных программ, экономит ваши клики мыши и делает менее вероятным установку нежелательных программ.',
+		'Unchecky автоматически снимает галочки дополнительных программ, экономя ваше время и делая менее вероятным случайную установку нежелательных программ.',
 	
 	'Warns':
 		'Предупреждает',
 	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
-		'Инсталляторы часто пытаются встроить установку дополнительных программ как естественную часть процесса установки. Unchecky предупреждает вас, когда вы пытаетесь согласиться на установку потенциально нежелательных пррограмм, что делает их случайную установку менее вероятной.',
+		'Установщики часто пытаются встроить установку дополнительных программ как естественную часть процесса установки. Unchecky предупреждает вас, когда вы пытаетесь согласиться на установку потенциально нежелательных программ, что делает их случайную установку менее вероятной.',
 	
 	'Updates automatically':
 		'Автоматически обновляется',
 	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
-		'Установил и забыл. Unchecky автоматически обновляется каждый раз, когда доступна новая версия, так что вам не придется беспокоиться о использовании актуальной версии.',
+		'Установил и забыл. Unchecky автоматически обновляется каждый раз, когда доступна новая версия, так что вам не придется беспокоиться об использовании актуальной версии.',
 	
 	'Watch Unchecky in action':
 		'Посмотри Unchecky в действии',
 	'<a href="http://unchecky.com/press#quotes">See what people are saying about Unchecky</a> or <a href="http://unchecky.com/press#videos">watch more videos</a>.':
-		'<a href="http://unchecky.com/press#quotes">Посмотри что говорят о Unchecky люди</a> или <a href="http://unchecky.com/press#videos">взгляни на видео</a>.',
+		'<a href="http://unchecky.com/press#quotes">Посмотри что говорят об Unchecky люди</a> или <a href="http://unchecky.com/press#videos">взгляни на видео</a>.',
 	
 	// Press
 	
 	'People are talking about Unchecky':
-		'Люди говорят о Unchecky',
+		'Люди говорят об Unchecky',
 	'Unchecky was also featured on the following international websites:':
-		'Unchecky была также представлена на следующих международных вебсайтах:',
+		'Программа Unchecky была также представлена на следующих международных вебсайтах:',
 	'on':
-		'',
-	'Награды':
-		'',
+		'на',
+	'Awards':
+		'Награды',
 	
 	'People are showing Unchecky in action':
 		'Люди демонстрируют Unchecky в действии',
 	'Demonstrating Unchecky':
-		'Демонстрация Uncheky',
+		'Демонстрация Unchecky',
 	
 	'Portuguese':
 		'Португальский',
@@ -89,7 +89,7 @@ jquery_lang_js.prototype.lang.ru = {
 	'Italian':
 		'Итальянский',
 	'French':
-		'Французкий',
+		'Французский',
 	
 	// FAQ
 	
@@ -101,12 +101,12 @@ jquery_lang_js.prototype.lang.ru = {
 	'My antivirus flags Unchecky as malicious.':
 		'Мой антивирус считает Unchecky вредоносным.',
 	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
-		'Это ложное срабатывание. Пожалуйста, сообщите об этом вашей антивирусной компании. Обратите внимание, что Unchecky была тщательно проверена Softpedia и <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">была признана на 100% безвредной.</a>',
+		'Это ложное срабатывание. Пожалуйста, сообщите об этом вашей антивирусной компании. Обратите внимание, что Unchecky была тщательно проверена компанией Softpedia и <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">была признана на 100% безвредной</a>.',
 	
 	'What is the best way to report a bug or suggest a feature?':
 		'Как сообщить об ошибке или внести предложение по программе?',
 	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
-		'Используйте страницу  <a href="http://unchecky.userecho.com/">Обратной связи</a>.',
+		'Используйте <a href="http://unchecky.userecho.com/">страницу обратной связи</a>.',
 	
 	'I want to translate Unchecky.':
 		'Я хочу перевести Unchecky.',
@@ -116,25 +116,25 @@ jquery_lang_js.prototype.lang.ru = {
 	// Contact
 	
 	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
-		'',
+		'Пожалуйста, не используйте данную форму обратной связи для сообщения об ошибках. Используйте для этого <a href="http://unchecky.userecho.com/">страницу UserEcho</a>.',
 	
 	'First Name':
 		'Имя',
 	'Last Name':
 		'Фамилия',
 	'Email Address':
-		'Email адрес',
+		'Электронная почта',
 	'Subject':
 		'Тема',
 	'Message':
 		'Сообщение',
 	'Send':
-		'Послать',
+		'Отправить',
 		
 	'Please fill in all fields.':
-		'Пожалуйста заполните все поля',
+		'Пожалуйста заполните все поля.',
 	'Please enter a valid email address.':
-		'Пожалуйста введите действующий email адрес',
+		'Пожалуйста введите действующий адрес электронной почты.',
 	'Could not complete your request.':
 		'Не удалось выполнить ваш запрос.',
 	'The message has been sent successfully.':
@@ -146,6 +146,6 @@ jquery_lang_js.prototype.lang.ru = {
 		'Не найдено',
 	
 	'Sorry but the page you are looking for cannot be found.':
-		'Жаль, но страница, которую вы ищете, не может быть найдена.',
+		'Сожалеем, но страница, которую вы ищете, не может быть найдена.',
 	
 }
