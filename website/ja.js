ba5ff77246441658d1a89295d37d3f2b90b3548b
RaMMicHaeL <leahcimmar@gmail.com>
2014-01-04 01:13:01 +0200

    Website: Japanese translation

---
+++ website/ja.js
@@ -0,0 +1,153 @@
+﻿jquery_lang_js.prototype.lang.ja = {
+	
+	// Header
+	
+	'Home':
+		'ホーム',
+	'Press':
+		'プレス',
+	'FAQ':
+		'よくある質問',
+	'Changelog':
+		'',
+	'Contact':
+		'コンタクト',
+	'Links':
+		'リンク',
+	'Bug/feature tracker':
+		'バグ/フィーチャー・トラッカー',
+	'Blog':
+		'ブログ',
+	'Donate':
+		'寄付',
+	'Language':
+		'言語',
+	
+	// Footer
+	
+	'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. 日本語訳： <a href="mailto:milksizegene@gmail.com">milksizegene</a>',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'ソフトウェアのインストール中、関連性のないオファーのチェックを外すのはうんざりですか？',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'ソフトウェアのインストール中に不要なプログラムのチェックを外してインストールすることを煩わしく感じたことはありませんか？ またチェックを外し忘れ、その後にアドウェアを削除する時間を要したことはありませんか？さらにプログラムをインストール後、ブラウザを開いた際にホーム設定当が改ざんされたということはありませんか？<br>Uncheckyはお使いのコンピューターに、不要なプログラムを導入させないことを目指しています。',
+	'Download':
+		'ダウンロード',
+	
+	'Introducing Unchecky!':
+		'Uncheckyをご紹介！',
+	'Keeps your checkboxes clear.':
+		'チェックボックスを除去します。',
+	
+	'Unchecks':
+		'アンチェック',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Uncheckyは自動的に関連性のないオファーのチェックを外すため、あなたがマウスをクリックする手間を省き、さらにチェックを外し忘れるというリスクを軽減することが出来ます。',
+	
+	'Warns':
+		'警告する',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'インストーラーは多くの場合、自然な形で追加プログラムのインストールを行います。Uncheckyはあなたが不要なオファーを許可しようとした際に警告を表示し、誤って不要なオファーを受け入れてしまうリスクを軽減させます。',
+	
+	'Updates automatically':
+		'自動更新',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'インストールして忘れてしまった。しかしUncheckyは新しいバージョンが公開された際に自動的に更新されるため、いつでも最新の状態を保つことが出来るので心配はいりません。',
+	
+	'Watch Unchecky in action':
+		'アクションでUncheckyを見る',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#引用">Uncheckyについてのコメントを見る</a> あるいは <a href="press#ビデオ">詳細なビデオを見る</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Uncheckyについての話題',
+	'Unchecky was also featured on the following international websites:':
+		'Uncheckyは以下のウェブサイト上で紹介されました：',
+	'on':
+		'上',
+	'Awards':
+		'アワード',
+	
+	'People are showing Unchecky in action':
+		'人々はアクションでUncheckyを見せている',
+	'Demonstrating Unchecky':
+		'Uncheckyの実証',
+	
+	'Portuguese':
+		'',
+	'Polish':
+		'',
+	'Russian':
+		'',
+	'Spanish':
+		'',
+	'Italian':
+		'',
+	'French':
+		'',
+	'German':
+		'',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Uncheckyのシステム要件は何ですか？',
+	'Unchecky supports Windows XP and above.':
+		'UncheckyはWindows XPおよび上記のバージョンをサポートしています。',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'セキュリティソフトにより、Uncheckyがマルウェアとして検出されます。',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
+		'それは誤検出です。セキュリティベンダーに誤検出に関する報告を行ってください。UncheckyはSoftpediaにより徹底的にテストされ、その結果が示されています。<a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">100% 安全</a>',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'バグの報告や機能を提案するための最良の方法は何ですか？',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'あなたのご意見を投稿してください。<a href="http://unchecky.userecho.com/">ユーザーフォーラム</a>',
+	
+	'I want to translate Unchecky.':
+		'Uncheckyを翻訳したい。',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Uncheckyのプログラムとウェブサイトの翻訳ファイルはGitHubのリポジトリに掲載されています：<a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'バグの報告や機能改善等の提案に、このお問い合わせフォームを使用しないで下さい。左記に関するお問い合わせはユーザフォーラムに投稿してください。<a href="http://unchecky.userecho.com/">ユーザーフォーラム</a>',
+	
+	'First Name':
+		'苗字',
+	'Last Name':
+		'名前',
+	'Email Address':
+		'メールアドレス',
+	'Subject':
+		'件名',
+	'Message':
+		'メッセージ',
+	'Send':
+		'送信',
+	
+	'Please fill in all fields.':
+		'すべての欄に記入してください。',
+	'Please enter a valid email address.':
+		'有効なメールアドレスを入力してください。',
+	'Could not complete your request.':
+		'リクエストを完了できませんでした。',
+	'The message has been sent successfully.':
+		'メッセージは正常に送信されました。',
+	
+	// 404
+	
+	'Not found':
+		'見つかりません',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'申し訳ありませんが、あなたが探しているページが見つかりません。',
+	
+}
