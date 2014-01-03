2c0cd151efb0bad666fdc763ce0ab390c13da142
RaMMicHaeL <leahcimmar@gmail.com>
2014-01-03 20:11:56 +0200

    Website: Simplified Chinese

---
+++ website/zh_cn.js
@@ -0,0 +1,153 @@
+jquery_lang_js.prototype.lang.zh_cn = {
+	
+	// Header
+	
+	'Home':
+		'主页',
+	'Press':
+		'新闻报道',
+	'FAQ':
+		'常见问题解答',
+	'Changelog':
+		'更新日志',
+	'Contact':
+		'联系',
+	'Links':
+		'链接',
+	'Bug/feature tracker':
+		'错误/特性跟踪器',
+	'Blog':
+		'博客',
+	'Donate':
+		'捐助',
+	'Language':
+		'语言',
+	
+	// Footer
+	
+	'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'版权所有 © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>。',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'在安装软件时对取消不相关提议的勾选感到疲劳？',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'你是否感觉到安装程序在安装软件时试图不惜一切代价推送附加的不需要的程序？曾经错过了一个复选框，之后花费数小时移除广告软件？曾经在安装后打开浏览器，却发现你有了新的主页、新的搜索引擎，甚至是新的浏览器？<br>Unchecky 旨在使潜在的不需要的程序不侵入你的计算机。',
+	'Download':
+		'下载',
+	
+	'Introducing Unchecky!':
+		'介绍Unchecky！',
+	'Keeps your checkboxes clear.':
+		'使你的复选框保持空白。',
+	
+	'Unchecks':
+		'取消选择',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky自动取消不相关的提议的选择，使你不需点击鼠标，并不太可能错过不需要推送的复选框。',
+	
+	'Warns':
+		'警告',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'安装往往试图偷偷加入其它程序作为安装的一个自然组成部分。 Unchecky在当你尝试接受一个潜在有害的推送时警告你，这使得它不太可能被意外地接受。',
+	
+	'Updates automatically':
+		'自动更新',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'安装并忘记。每当有新版本可用时，Unchecky会自动更新，所以你不必担心运行最新版本。',
+	
+	'Watch Unchecky in action':
+		'观看Unchecky在行动',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">看看人们对于Unchecky说什么</a> 或 <a href="press#videos">看更多的视频</a>。',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'人们在谈论Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky也在以下国际网站上被描述：',
+	'on':
+		'在',
+	'Awards':
+		'奖项',
+	
+	'People are showing Unchecky in action':
+		'人们在行动中展示Unchecky',
+	'Demonstrating Unchecky':
+		'展示Unchecky',
+	
+	'Portuguese':
+		'葡萄牙语',
+	'Polish':
+		'波兰语',
+	'Russian':
+		'俄语',
+	'Spanish':
+		'西班牙语',
+	'Italian':
+		'意大利语',
+	'French':
+		'法语',
+	'German':
+		'德语',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Unchecky的系统要求是什么？',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky支持Windows XP和更新的系统。',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'我的反病毒软件标志Unchecky为恶意。',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
+		'它是误报。请向你的反病毒软件公司报告。请注意，Unchecky已被<a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">完全测试，是100%无害的</a>。',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'报告错或提出特性建议的最好的方法是什么？',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'在<a href="http://unchecky.userecho.com/">用户回声页面</a>发表你的反馈。',
+	
+	'I want to translate Unchecky.':
+		'我想要翻译Unchecky。',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Unchecky程序和网站的翻译文件可以在<a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-翻译</a>GitHub资料库找到。',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'请不要使用此联系表格来报告错误或提出特性的建议。请在<a href="http://unchecky.userecho.com/">用户回声页面</a>发表你的反馈。',
+	
+	'First Name':
+		'名',
+	'Last Name':
+		'姓',
+	'Email Address':
+		'电子邮件地址',
+	'Subject':
+		'主题',
+	'Message':
+		'消息',
+	'Send':
+		'发送',
+		
+	'Please fill in all fields.':
+		'请填写所有字段。',
+	'Please enter a valid email address.':
+		'请输入有效的电子邮件地址。',
+	'Could not complete your request.':
+		'不能完成你的请求。',
+	'The message has been sent successfully.':
+		'消息已成功发送。',
+	
+	// 404
+	
+	'Not found':
+		'未找到',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'抱歉，但找不到您寻找的页面。',
+	
+}
