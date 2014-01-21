6bebc47cb5fc9696786120a53293d710db24cb5a
JohnKiller <gianni4592@gmail.com>
2014-01-21 12:35:15 -0800

    Italian translation for the Unchecky website

---
+++ website/it.js
@@ -0,0 +1,153 @@
+jquery_lang_js.prototype.lang.fr = {
+	
+	// Header
+	
+	'Home':
+		'Home',
+	'Press':
+		'Recensioni',
+	'FAQ':
+		'FAQ',
+	'Changelog':
+		'Changelog',
+	'Contact':
+		'Contatti',
+	'Links':
+		'Collegamenti',
+	'Bug/feature tracker':
+		'Segnalazioni e richieste',
+	'Blog':
+		'Blog',
+	'Donate':
+		'Donazioni',
+	'Language':
+		'Lingua',
+	
+	// Footer
+	
+	'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. Traduzione italiana : <a href="mailto:gianni4592@gmail.com">Gianni Angelozzi</a>',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Stanco di deselezionare le pubblicità mentre installi qualche programma?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Hai mai avuto la sensazione che, mentre installavi qualcosa, il programma facesse di tutto per installare ulteriori programmi non voluti? Hai mai tralasciato una checkbox, e poi hai speso ore rimuovendo adaware dal tuo pc? Hai mai aperto il tuo browser dopo un\'installazione trovando una nuova homepage, un nuovo provider di ricerca o proprio un nuovo browser?<br />Unchecky è il programma che cerca di bloccare l\'installazione di programmi potenzialmente non voluti.',
+	'Download':
+		'Download',
+	
+	'Introducing Unchecky!':
+		'Ecco a voi Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Mantiene le checkbox pulite.',
+	
+	'Unchecks':
+		'Deseleziona',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky disattiva in modo automatico tutte le offerte di programmi estranei, così da risparmiare click del mouse e facendo si che sia più difficile dimenticarsi qualche checkbox.',
+	
+	'Warns':
+		'Avverte',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Alcune installazioni cercano di installare programmi aggiuntivi durante il normale processo di installazione. Unchecky ti avverte quando provi ad accettare una di queste offerte, così che sia più difficile cliccarci accidentalmente.',
+	
+	'Updates automatically':
+		'Si aggiorna da solo',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Installalo e dimenticatene. Unchecky è in grado di aggiornarsi nonappena una nuova versione è disponibile, così che tu non debba preoccuparti di installare sempre l\'ultima versione',
+	
+	'Watch Unchecky in action':
+		'Guarda Unchecky in azione',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Guarda cosa si dice di Unchecky</a> oppure <a href="press#videos">guarda altri video</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Parlano di Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky è stato mensionato in questi siti internazionali:',
+	'on':
+		'su',
+	'Awards':
+		'Premi',
+	
+	'People are showing Unchecky in action':
+		'Persone che mostrano Unchecky in azione:',
+	'Demonstrating Unchecky':
+		'Dimostrazione di Unchecky',
+	
+	'Portuguese':
+		'Portoghese',
+	'Polish':
+		'Polacco',
+	'Russian':
+		'Russo',
+	'Spanish':
+		'Spagnolo',
+	'Italian':
+		'Italiano',
+	'French':
+		'Francese',
+	'German':
+		'Tedesco',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Quali sono i requisiti di sistema per usare Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky supporta tutte le versioni di Windows a partire da XP.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Il mio antivirus mi segnala Unchecky come malevolo/virus.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
+		'È un falso positivo. Per favore, segnalalo al produttore del tuo antivirus. Unchecky è stato testato profondamente da Softpedia ed <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">è stato dichiarato pulito al 100%</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Qual\'è il modo migliore per segnalare un bug o suggerire una modifica?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Scrivi i tuoi commenti sulla <a href="http://unchecky.userecho.com/">pagina UserEcho</a>',
+	
+	'I want to translate Unchecky.':
+		'Vorrei tradurre Unchecky nella mia lingua.',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Le traduzioni del programma e del sito web di Unchecky sono disponibili su GitHub nella repo <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Per favore non utilizzare questa pagina per eventuali bug o richieste. Invece, scrivi i tuoi commenti sulla <a href="http://unchecky.userecho.com/">pagina UserEcho</a>.',
+	
+	'First Name':
+		'Nome',
+	'Last Name':
+		'Cognome',
+	'Email Address':
+		'E-Mail',
+	'Subject':
+		'Soggetto',
+	'Message':
+		'Messaggio',
+	'Send':
+		'Invia',
+		
+	'Please fill in all fields.':
+		'Per favore inserisci tutti i dati',
+	'Please enter a valid email address.':
+		'Per favore inserisci un indirizzo email valido',
+	'Could not complete your request.':
+		'Non sono riuscito ad inviare il tuo messaggio.',
+	'The message has been sent successfully.':
+		'Il messaggio è stato inviato correttamente.',
+	
+	// 404
+	
+	'Not found':
+		'Pagina non trovata',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Mi dispiace, ma la pagina che stai cercando non esiste.',
+	
+}
