1a2689e3466f81f512a06054af046cd254151e03
RaMMicHaeL <leahcimmar@gmail.com>
2014-04-14 00:54:30 +0300

    Norwegian translation of the website

---
+++ website/nb_no.js
@@ -0,0 +1,153 @@
+jquery_lang_js.prototype.lang.nb_no = {
+	
+	// Header
+	
+	'Home':
+		'Hjem',
+	'Press':
+		'Presse',
+	'FAQ':
+		'FAQ',
+	'Changelog':
+		'Endringslogg',
+	'Contact':
+		'Kontakt',
+	'Links':
+		'Lenker',
+	'Bug/feature tracker':
+		'Bug/funksjons-sporer',
+	'Blog':
+		'Blogg',
+	'Donate':
+		'Doner',
+	'Language':
+		'Språk',
+	
+	// Footer
+	
+	'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. Norsk oversettelse: <a href="mailto:alexander@feskenet.com?Subject=Unchecky">Alexander Johansen</a>',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Lei av å fjerne avhukinger for urelaterte tilbud når du installerer programvare?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Har du noen gang følt at, når du installerer programmvare, blir du forsøkt lurt til å installere tilleggsprogrammer du ikke ønsker? Har du noen gang ved et uhell ikke fjernet en avhuking av en sjekkboks, og brukt flere timer etterpå for å fjerne reklame-programvare? Har du noensinne åpnet nettleseren etter en installasjon og oppdaget at du har en ny startside, en ny søkemotor eller til og med en ny nettleser?<br>Unchecky forsøker å holde potensielt uønskede programmer unna din datamaskin.',
+	'Download':
+		'Last ned',
+	
+	'Introducing Unchecky!':
+		'Vi presenterer Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Tømmer dine sjekkbokser.',
+	
+	'Unchecks':
+		'Krysser vekk',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky fjerner automatisk avkryssing for urelaterte tilbud. Den sparer deg for flere museklikk og gjør det mindre sannsynlig å miste en sjekkboks for potensielt uønsket tilbud.',
+	
+	'Warns':
+		'Advarer',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Installasjoner prøver ofte å lure inn ekstra programmvare som en naturlig del av installasjonen. Unchecky advarer deg når du prøver å akseptere et potensielt uønsket tilbud, noe som gjør at det er mindre sannsynlig for å akseptere ved et uhell.',
+	
+	'Updates automatically':
+		'Oppdateres automatisk',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Installer og glem. Unchecky oppdateres automatisk til en hver tid når en ny versjon er tilgjengelig, slik at du ikke trenger å bekymre deg om du kjører siste versjon.',
+	
+	'Watch Unchecky in action':
+		'Se Unchecky i praksis',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Se hva folk sier om Unchecky</a> eller <a href="press#videos">se flere videoer</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Det snakkes om Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky var også kjennetegnet på følgende internasjonale websider:',
+	'on':
+		'på',
+	'Awards':
+		'Utmerkelser',
+	
+	'People are showing Unchecky in action':
+		'Folk viser Unchecky i aksjon',
+	'Demonstrating Unchecky':
+		'Demonstrasjon av Unchecky',
+	
+	'Portuguese':
+		'Portugisisk',
+	'Polish':
+		'Polsk',
+	'Russian':
+		'Russisk',
+	'Spanish':
+		'Spansk',
+	'Italian':
+		'Italiensk',
+	'French':
+		'Fransk',
+	'German':
+		'Tysk',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Hva med systemkravene for Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky støtter Windows XP og senere.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Mitt antivirus program sier at Unchecky er skadelig.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
+		'Det er en falsk positiv. Vennligst rapporter dette til ditt antivirus selskap. Merk at Unchecky var nøye gjennomtestet av Softpedia og <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">funnet som 100% trygt</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Hva er den beste måten å rapportere om en bug eller foreslå nye funksjoner?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Send dine tilbakemeldinger på <a href="http://unchecky.userecho.com/">UserEcho</a> sidene.',
+	
+	'I want to translate Unchecky.':
+		'Jeg ønsker å oversette Unchecky.',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Oversettnings-filene til Unchecky programmet og websiden kan du finne på GitHub: <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Vennligst ikke bruk dette kontaktskjemaet til å melde om feil eller foreslå nye funksjoner. Send dine tilbakemeldinger på <a href="http://unchecky.userecho.com/">UserEcho</a> sidene.',
+	
+	'First Name':
+		'Fornavn',
+	'Last Name':
+		'Etternavn',
+	'Email Address':
+		'E-post',
+	'Subject':
+		'Emne',
+	'Message':
+		'Melding',
+	'Send':
+		'Send',
+		
+	'Please fill in all fields.':
+		'Vennligst fyll ut alle feltene.',
+	'Please enter a valid email address.':
+		'Vennligst skriv inn en gyldig e-postadresse.',
+	'Could not complete your request.':
+		'Din forespørsel kunne ikke fullføres.',
+	'The message has been sent successfully.':
+		'Din melding har blitt sendt.',
+	
+	// 404
+	
+	'Not found':
+		'Siden finnes ikke',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Beklager, siden du ser etter kan ikke bli funnet.',
+	
+}
