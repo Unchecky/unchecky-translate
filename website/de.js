72998b38e0c380be5ff0ca8df6d1a5e89e50394c
Felix Kiunke <mail@fkiunke.de>
2014-06-21 15:11:32 +0200

    Improve grammar and general translation

--- website/de.js
+++ website/de.js
@@ -15,7 +15,7 @@ jquery_lang_js.prototype.lang.de = {
 	'Links':
 		'Links',
 	'Bug/feature tracker':
-		'Bug/feature tracker',
+		'Bug-/Feature-Tracker',
 	'Blog':
 		'Blog',
 	'Donate':
@@ -26,46 +26,46 @@ jquery_lang_js.prototype.lang.de = {
 	// Footer
 	
 	'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
-		'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. Deutsche Übersetzung: <a href="mailto:lechuck@mailueberfall.de?Subject=Unchecky">LeChuck87</a>.',
+		'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. Deutsche Übersetzung: <a href="mailto:lechuck@mailueberfall.de?Subject=Unchecky">LeChuck87</a>, Felix Kiunke.',
 	
 	// Home
 	
 	'Tired of unchecking unrelated offers while installing software?':
-		'Du bist es satt Häkchen von ungewollten Angeboten während einer Softwareinstallation zu entfernen?',
+		'Du hast es satt, Häkchen von ungewollten Angeboten zu entfernen, wenn du Software installierst?',
 	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
-		'Hast du das Gefühl, dass die Installation mit aller Macht versucht ungewollte Programme mitzuinstallieren? Schon einmal ein Häkchen vergessen und danach stundenlang versucht die Adware zu deinstallieren? Nach einer Installation den Webbrowser geöffnet und eine neue Homepage vorgefunden? Eine neue Suchmaschine? Oder gar ein ganz neuer Browser?<br>Unchecky versucht potentiell ungewollte Software vom Computer fernzuhalten.',
+		'Hast du das Gefühl, dass der Installer mit aller Macht versucht, zusätzliche Programme mitzuinstallieren? Schon einmal ein Häkchen vergessen und danach stundenlang Adware deinstalliert? Nach einer Installation den Webbrowser geöffnet und eine neue Homepage vorgefunden? Eine neue Suchmaschine? Oder gar ein ganz neuer Browser?<br>Unchecky versucht, potentiell ungewollte Software von deinem Computer fernzuhalten.',
 	'Download':
-		'Downloaden',
+		'Herunterladen',
 	
 	'Introducing Unchecky!':
 		'Entdecke Unchecky!',
 	'Keeps your checkboxes clear.':
-		'Hält deine Häkchen zurück!',
+		'Hält deine Checkboxen frei.',
 	
 	'Unchecks':
 		'Entfernt Häkchen',
 	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
-		'Unchecky entfernt automatisch Häkchen von ungewollten Angeboten. Es spart Mausklicks ein und es ist weniger wahrscheinlich das du ein Häkchen bei ungewollten Softwareangeboten vergisst.',
+		'Unchecky entfernt automatisch Häkchen von ungewollten Angeboten und spart dir Mausklicks. Du wirst nie wieder ein Häkchen vergessen.',
 	
 	'Warns':
-		'Warnt',
+		'Warnt dich',
 	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
-		'Installationen versuchen oft weitere Programme während der Installation mit einzuschleusen. Unchecky warnt dich wenn du versucht potentiell ungewollte Angebote anzunehmen. Dadurch ist die Gefahr einer versehentlichen Installation der ungewollten Software geringer.',
+		'Installationen versuchen oft, weitere Programme während der Installation mit einzuschleusen. Unchecky warnt dich, wenn du versehentlich potentiell ungewollte Angebote annimmst. Dadurch ist die Gefahr geringer, dass du versehentlich unerwünschte Programme mitinstallierst.',
 	
 	'Updates automatically':
-		'Updatet automatisch',
+		'Automatische Updates',
 	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
-		'Installieren und vergessen. Unchecky updatet automatisch wenn eine neue Version zur Verfügung steht. Du musst dich nicht um die neueste Version kümmern.',
+		'Installieren und vergessen. Unchecky updatet automatisch, sobald eine neue Version zur Verfügung steht, sodass du dich nie wieder um die neueste Version kümmern musst.',
 	
 	'Watch Unchecky in action':
-		'Schaue dir Unchecky in Aktion an',
+		'Unchecky in Aktion',
 	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
-		'<a href="press#quotes">Was andere Leute über Unchecky sagen</a> oder <a href="press#videos">Schaue weitere Videos</a>.',
+		'<a href="press#quotes">Lies, was andere Leute über Unchecky sagen</a> oder <a href="press#videos">schau dir weitere Videos an</a>.',
 	
 	// Press
 	
 	'People are talking about Unchecky':
-		'Leute sprechen über Unchecky',
+		'Was andere über Unchecky sagen',
 	'Unchecky was also featured on the following international websites:':
 		'Unchecky wurde auf folgenden internationalen Webseiten erwähnt:',
 	'on':
@@ -74,58 +74,58 @@ jquery_lang_js.prototype.lang.de = {
 		'Preise:',
 	
 	'People are showing Unchecky in action':
-		'Menschen zeigen Unchecky in Aktion',
+		'Unchecky in Aktion',
 	'Demonstrating Unchecky':
-		'Demonstration von Unchecky',
+		'Vorführung von Unchecky',
 	
 	'Portuguese':
-		'Portugiesisch',
+		'portugiesisch',
 	'Polish':
-		'Polnisch',
+		'polnisch',
 	'Russian':
-		'Russisch',
+		'russisch',
 	'Spanish':
-		'Spanisch',
+		'spanisch',
 	'Italian':
-		'Italienisch',
+		'italienisch',
 	'French':
-		'Französisch',
+		'französisch',
 	'German':
-		'Deutsch',
+		'deutsch',
 	
 	// FAQ
 	
 	'What are the system requirements for Unchecky?':
-		'Was sind die Systemvorraussetzungen von Unchecky?',
+		'Welche Systemvorraussetzungen hat Unchecky?',
 	'Unchecky supports Windows XP and above.':
 		'Unchecky läuft auf Windows XP und neuer.',
 	
 	'My antivirus flags Unchecky as malicious.':
-		'Mein Virenschutz meldet Unchecky als gefährtlich.',
+		'Mein Virenschutz meldet Unchecky als gefährlich.',
 	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
-		'Unchecky wird fälschlicherweise erkannt. Bitte melde diesen Feher dem Virenschutzhersteller. Bitte beachte das Unchecky umfangreich von Softpedia getestet wurde und<a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">zu 100% ungefährlich ist</a>.',
+		'Manche Virenscanner erkennen Unchecky zu Unrecht als Virus. Bitte melde diesen Fehler dem Hersteller deines Antivirenprogramms. Unchecky wurde umfangreich von Softpedia getestet und <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">ist absolut virenfrei</a>.',
 	
 	'What is the best way to report a bug or suggest a feature?':
-		'Wie kann ich am besten einen Fehler melden oder Verbesserungen einreichen?',
+		'Wie kann ich am besten einen Fehler melden oder Verbesserungen vorschlagen?',
 	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
-		'Schreibe ein Feedback auf der folgenden <a href="http://unchecky.userecho.com/">UserEcho Seite</a>.',
+		'Schreib uns dein Feedback auf der <a href="http://unchecky.userecho.com/">UserEcho-Seite</a>.',
 	
 	'I want to translate Unchecky.':
-		'Ich will Unchecky übersetzen.',
+		'Ich will Unchecky übersetzen!',
 	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
-		'Die Übersetzungsdateien des Programms sowie der Webseite können auf dem <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub Repository gefunden werden.',
+		'Die Übersetzungsdateien des Programms sowie der Webseite können im <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>-GitHub-Repository gefunden werden.',
 	
 	// Contact
 	
 	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
-		'Bitte benutze dieses Kontaktformular nicht für Fehlermeldungen oder Verbesserungsvorschläge. Feedback kann auf dieser <a href="http://unchecky.userecho.com/">UserEcho Seite</a> abgegeben werden.',
+		'Bitte benutze dieses Kontaktformular nicht für Fehlermeldungen oder Verbesserungsvorschläge. Feedback kann auf der <a href="http://unchecky.userecho.com/">UserEcho-Seite</a> eingereicht werden.',
 	
 	'First Name':
 		'Vorname',
 	'Last Name':
 		'Nachname',
 	'Email Address':
-		'E-Mail Adresse',
+		'E-Mail-Adresse',
 	'Subject':
 		'Betreff',
 	'Message':
@@ -134,13 +134,13 @@ jquery_lang_js.prototype.lang.de = {
 		'Senden',
 		
 	'Please fill in all fields.':
-		'Bitte fülle alle Felder aus.',
+		'Bitte fülle alle Felder aus!',
 	'Please enter a valid email address.':
-		'Bitte eine korrekte E-Mail Adresse angeben.',
+		'Bitte gib eine gültige E-Mail-Adresse an!',
 	'Could not complete your request.':
-		'Konnte die Anfrage nicht abschließen.',
+		'Die Nachricht konnte nicht abgeschickt werden.',
 	'The message has been sent successfully.':
-		'Die Nachricht wurde erfolgreich versendet.',
+		'Deine Nachricht wurde erfolgreich versendet!',
 	
 	// 404
 	
@@ -148,6 +148,6 @@ jquery_lang_js.prototype.lang.de = {
 		'Nicht gefunden',
 	
 	'Sorry but the page you are looking for cannot be found.':
-		'Entschuldigung - Die gesuchte Seite konnte nicht gefunden werden.',
+		'Entschuldigung – die gesuchte Seite konnte nicht gefunden werden.',
 	
 }
