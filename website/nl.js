63d147a23b05f8199a0ca5eb4b6385f0fe83334e
RaMMicHaeL <leahcimmar@gmail.com>
2014-07-26 22:00:30 +0300

    Removed Dutch (Belgium) and nl_be

--- website/nl.js
+++ website/nl.js
@@ -1,4 +1,4 @@
-jquery_lang_js.prototype.lang.nl_be = {
+jquery_lang_js.prototype.lang.nl = {
 	
 	// Header
 	
@@ -19,43 +19,43 @@ jquery_lang_js.prototype.lang.nl_be = {
 	'Blog':
 		'Blog',
 	'Donate':
-		'Steun',
+		'Doneren',
 	'Language':
 		'Taal',
 	
 	// Footer
 	
 	'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
-		'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. Nederlandse vertaling: <a href="mailto:contact@studio384.be?Subject=Unchecky">Studio 384</a>.',
+		'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. Nederlandse vertaling: <a href="mailto:contact@studio384.be?Subject=Unchecky">Studio 384</a> en <a href="mailto:unchecky@marchulsebosch.nl>Marc</a>.',
 	
 	// Home
 	
 	'Tired of unchecking unrelated offers while installing software?':
-		'Ben je het moe om ongewenste software uit te vinken tijdens installaties?',
+		'Bent u het beu om ongewenste software uit te vinken tijdens installaties?',
 	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
-		'Heb je ooit gezien dat er extra software bij installaties wordt meegelevert welke kost wat kost geïnstalleerd willen worden? Ooit een selectievak gemist, om vervolgens uren \'plezie\' te hebben met het verwijderen van adware? Ooit je browser gestart na een installatie, om te ontdekken dat je startpagina is veranderd, je gebruik maakt van een nieuwe zoekmachine of zelfs een nieuwe browser?<br>Unchecky wilt je beschermen tegen mogelijk ongewenste software op uw computer.',
+		'Heeft u ooit gezien dat er extra software bij installaties wordt meegeleverd die koste wat kost geïnstalleerd wil worden? Ooit een selectievak gemist, om vervolgens uren \'plezier\' te hebben met het verwijderen van adware? Ooit uw browser gestart na een installatie, om te ontdekken dat uw startpagina is veranderd, u gebruik maakt van een nieuwe zoekmachine of zelfs een nieuwe browser?<br>Unchecky heeft als doel u te beschermen tegen mogelijk ongewenste software op uw computer.',
 	'Download':
 		'Download',
 	
 	'Introducing Unchecky!':
 		'Ontdek Unchecky!',
 	'Keeps your checkboxes clear.':
-		'Hou je selectievakjes schoon!',
+		'Houdt uw selectievakjes schoon!',
 	
 	'Unchecks':
-		'Uitvinken',
+		'Vinkt uit',
 	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
-		'Unchecky schakelt de selectievakjes van ongerelateerde software automatisch uit, het bespaart muis kliks en voorkomt dat je er mist.',
+		'Unchecky schakelt de selectievakjes van ongerelateerde software automatisch uit,  bespaart muiskliks en voorkomt dat u vakjes mist.',
 	
 	'Warns':
-		'Waarschuwingen',
+		'Waarschuwt',
 	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
-		'Sommige installaties proberen soms extra software te installeren. Unchecky waarschuwt je als je zo\'n aanbieding van ongewenste software aanvaard.',
+		'Sommige installaties proberen extra software te installeren. Unchecky waarschuwt u als u op het punt staat zo\'n aanbieding van ongewenste software te aanvaarden.',
 	
 	'Updates automatically':
 		'Update automatisch',
 	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
-		'Installeer en vergeet. Unchecky houd zichzelf up-to-date, je hoeft je dus geen zorgen te maken over de laatste nieuwe versie.',
+		'Installeer en vergeet. Unchecky houdt zichzelf up-to-date, u hoeft zich dus geen zorgen te maken over de nieuwste versie.',
 	
 	'Watch Unchecky in action':
 		'Zie Unchecky in actie',
@@ -65,9 +65,9 @@ jquery_lang_js.prototype.lang.nl_be = {
 	// Press
 	
 	'People are talking about Unchecky':
-		'Mensen praten over Unchecky',
+		'Wat mensen zeggen over Unchecky',
 	'Unchecky was also featured on the following international websites:':
-		'Unchecky werd also getoond op volgende internationale websites:',
+		'Unchecky werd uitgelicht op volgende internationale websites:',
 	'on':
 		'op',
 	'Awards':
@@ -98,34 +98,34 @@ jquery_lang_js.prototype.lang.nl_be = {
 	'What are the system requirements for Unchecky?':
 		'Wat zijn de systeemvereisten voor Unchecky?',
 	'Unchecky supports Windows XP and above.':
-		'Unchecky ondersteund Windows XP en hoger.',
+		'Unchecky ondersteunt Windows XP en hoger.',
 	
 	'My antivirus flags Unchecky as malicious.':
 		'Mijn anti-virus meld dat Unchecky onbetrouwbaar is.',
 	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
-		'Dat is een false-positive. Je kan deze fout raporteren bij het bedrijf dat je anti-virus ontwikkelt. Unchecky is getest door Softpedia en <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">is 100% schoon</a>.',
+		'Loos alarm. U kunt deze fout rapporteren bij het bedrijf dat uw anti-virus ontwikkelt. Unchecky is getest door Softpedia en <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">is 100% schoon</a>.',
 	
 	'What is the best way to report a bug or suggest a feature?':
 		'Hoe kan ik het best een bug raporteren of een functie aanvragen?',
 	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
-		'Plaats je feedback op de <a href="http://unchecky.userecho.com/">UserEcho pagina</a>.',
+		'Plaats uw feedback op de <a href="http://unchecky.userecho.com/">UserEcho pagina</a>.',
 	
 	'I want to translate Unchecky.':
 		'Ik wil Unchecky vertalen.',
 	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
-		'De vertalingen van Unchecky en de website kan je vinden in de  <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository',
+		'De vertalingen van Unchecky en de website kunt u vinden in de  <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository',
 	
 	// Contact
 	
 	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
-		'Gebruik dit contact formulier niet op bugs te raporteren of functies aan te vragen. Plaats feedback op de <a href="http://unchecky.userecho.com/">UserEcho pagina</a>.',
+		'Gebruik dit contactformulier niet op bugs te raporteren of functies aan te vragen. Plaats feedback op de <a href="http://unchecky.userecho.com/">UserEcho pagina</a>.',
 	
 	'First Name':
 		'Voornaam',
 	'Last Name':
 		'Achternaam',
 	'Email Address':
-		'Email adres',
+		'Email-adres',
 	'Subject':
 		'Onderwerp',
 	'Message':
@@ -136,11 +136,11 @@ jquery_lang_js.prototype.lang.nl_be = {
 	'Please fill in all fields.':
 		'Vul alle velden in',
 	'Please enter a valid email address.':
-		'Geef een correct email adres.',
+		'Vul een correct email-adres in.',
 	'Could not complete your request.':
-		'Kan je aanvraag niet voltooien',
+		'Kan uw aanvraag niet voltooien',
 	'The message has been sent successfully.':
-		'Je bericht is met succes verzonden',
+		'Uw bericht is met succes verzonden',
 	
 	// 404
 	
@@ -148,6 +148,6 @@ jquery_lang_js.prototype.lang.nl_be = {
 		'Niet gevonden',
 	
 	'Sorry but the page you are looking for cannot be found.':
-		'Sorry, de pagina die je zoekt is niet beschikbaar.',
+		'Sorry, de pagina die u zoekt is niet beschikbaar.',
 	
 }
