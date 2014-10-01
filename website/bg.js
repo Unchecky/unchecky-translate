9b29225c5a896fae286a4c8810c04fc72f3d215a
Michael Maltsev <leahcimmar@gmail.com>
2014-10-01 12:32:49 +0300

    Website: added Bulgarian translation

---
+++ website/bg.js
@@ -0,0 +1,158 @@
+jquery_lang_js.prototype.lang.bg = {
+	
+	// Header
+	
+	'Home':
+		'Начало',
+	'Press':
+		'Преса',
+	'FAQ':
+		'ЧЗВ',
+	'Changelog':
+		'Промени',
+	'Contact':
+		'Контакт',
+	'Links':
+		'Връзки',
+	'Bug/feature tracker':
+		'Тракер на бъгове/функции',
+	'Blog':
+		'Блог',
+	'Donate':
+		'Дарение',
+	'Language':
+		'Език',
+	
+	// Footer
+	
+	'Copyright © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Всички права запазени © 2014 <a href="http://rammichael.com/">RaMMicHaeL</a>. Български превод: Night_Raven.',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Писнало ли Ви е да размаркирате нежелани предложения за при инсталиране на софтуер?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Имали ли сте усещането, че докато инсталирате софтуер, инсталаторът се опитва всячески да Ви пробута допълнителни, нежелани програми? Пропускали ли сте квадратче с отметка и после да сте прекарали часове в премахване на рекламен софтуер? Отваряли ли сте браузъра след инсталация, за да установите, че имате нова начална страница, нова търсеща машина или дори напълно нов браузър?<br>Unchecky има за цел да държи всички тези нежелани програми вън от Вашия компютър.',
+	'Download':
+		'Изтегляне',
+	
+	'Introducing Unchecky!':
+		'Представяме Ви Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Поддържа квадратчетата за отметки празни.',
+	
+	'Unchecks':
+		'Премахва отметки',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky премахва автоматично отметки от нежелани предложения по време на инсталации, което намалява както броя кликания, така и вероятността да пропуснете квадратче за нежелана програма.',
+	
+	'Warns':
+		'Предупреждава Ви',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Инсталаторите често се опитват да пробутат допълнителни програми като част същинската инсталация. Unchecky Ви предупреждава, когато се прави опит да Ви се пробута потенциално нежелана програма, което намалява шансовете да я приемете неволно.',
+	
+	'Updates automatically':
+		'Актуализира се автоматично',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Инсталирате и забравяте. Unchecky се актуализира автоматично, когато е налична нова версия, за да не се грижите Вие за това.',
+	
+	'Watch Unchecky in action':
+		'Unchecky в действие',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Прочетете какво други потребители казват за Unchecky</a> или <a href="press#videos">гледайте още клипове</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Какво други потребители казват за Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky е представена на следните международни страници:',
+	'on':
+		'на',
+	'Awards':
+		'Отличия:',
+	
+	'People are showing Unchecky in action':
+		'Unchecky в действие',
+	'Demonstrating Unchecky':
+		'Демонстрация на Unchecky',
+	
+	'Portuguese':
+		'португалски',
+	'Polish':
+		'полски',
+	'Russian':
+		'руски',
+	'Spanish':
+		'испански',
+	'Italian':
+		'италиански',
+	'French':
+		'френски',
+	'German':
+		'немски',
+		
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Какви са системните изисквания на Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky поддържа Windows XP и всички след нея.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Моята антивирусна засича Unchecky като заплаха.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">was found 100% clean</a>.':
+		'Това е фалшива тревога. Моля, докладвайте това на компанията автор на Вашата антивирусна програма. Unchecky е тествана обстойно от Softpedia и <a href="http://www.softpedia.com/progClean/Unchecky-Clean-244234.html">е 100% чиста</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Какъв е най-добрият начин за докладване на проблем или за изискване на нова функция?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Пишете ни на <a href="http://unchecky.userecho.com/">UserEcho-Seite</a>.',
+	
+	'I want to translate Unchecky.':
+		'Искам да преведа Unchecky!',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Файловете за превод на самата програма и сайта на Unchecky могат да бъдат открити в <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>-GitHub хранилището.',
+	
+	'Is it possible to install Unchecky silently?':
+		'Възможно ли е да инсталирам Unchecky в тих режим?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Да, стартирайте инсталатора с <code>-install</code> параметър от командната лента. За пълен списък с поддържани параметри, стартирайте <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Моля, не използвайте тази форма за контакт, за да докладвате проблеми или да искате нови функции. За това използвайте <a href="http://unchecky.userecho.com/">UserEcho страницата</a>.',
+	
+	'First Name':
+		'Собствено име',
+	'Last Name':
+		'Фамилно име',
+	'Email Address':
+		'Електронен адрес',
+	'Subject':
+		'Заглавие',
+	'Message':
+		'Съобщение',
+	'Send':
+		'Изпрати',
+		
+	'Please fill in all fields.':
+		'Моля, попълнете всички полета!',
+	'Please enter a valid email address.':
+		'Моля, въведете валиден електронен адрес!',
+	'Could not complete your request.':
+		'Съобщението не беше изпратено.',
+	'The message has been sent successfully.':
+		'Съобщението беше изпратено успешно!',
+	
+	// 404
+	
+	'Not found':
+		'Не е открито',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Извиняваме се, но търсената от Вас страница не може да бъде открита.',
+	
+}
