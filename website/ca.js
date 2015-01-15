52b8ed5f2a916448574647c5b462513bc7ed964f
Michael Maltsev <leahcimmar@gmail.com>
2015-01-15 15:58:22 +0200

    Added Catalan translation of the program and the website

---
+++ website/ca.js
@@ -0,0 +1,185 @@
+jquery_lang_js.prototype.lang.ca = {
+	
+	// Header
+	
+	'Home':
+		'Inici',
+	'Press':
+		'Premsa',
+	'FAQ':
+		'PMF',
+	'Changelog':
+		'Notes de la versió',
+	'Contact':
+		'Contacte',
+	'Links':
+		'Enllaços',
+	'Bug/feature tracker':
+		'Errors i suggeriments',
+	'Blog':
+		'Bloc',
+	'Language':
+		'Idioma',
+	
+	// Footer
+	
+	'Copyright © 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'© 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>. Traducció a càrrec de: <a href="mailto:bennybeat+unchecky@gmail.com">BennyBeat [CATegoria]</a>',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Acord de llicència d\'usuari final',
+	'Privacy Policy':
+		'Política de privadesa',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Farts de desmarcar ofertes irrellevants en instal·lar programari?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Us ha passat alguna vegada que l\'instal·lador d\'una aplicació us ofereix programari que res té a veure amb el que esteu instal·lant? Heu perdut hores suprimint brossa de l\'equip "gràcies" a les caselles inintel·ligibles que venien marcades per defecte? Això per no parlar del segrest al navegador de la pàgina d\'inici, canvis al motor de cerca, o fins i tot la instal·lació d\'un nou que esdevingui el navegador per defecte... Unchecky és una utilitat que té com a objectiu evitar la instal·lació d\'aplicacions potencialment perilloses a l\'ordinador.',
+	'Download':
+		'Baixada gratuïta',
+	
+	'Introducing Unchecky!':
+		'Us presentem l\'Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Manté a ratlla les instal·lacions no desitjades.',
+	
+	'Unchecks':
+		'Desmarca',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'L\'Unchecky inhabilita automàticament les ofertes no relacionades, estalviant clics del ratolí i el més important; protegint-lo contra aplicacions potencialment perilloses.',
+	
+	'Warns':
+		'Avisa',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Els instal·ladors sovint intenten afegir programari addicional com una part natural de la instal·lació. L\'Unchecky us avisa en trobar una oferta potencialment no desitjada, minimitzant els riscos que siguin acceptades per accident.',
+	
+	'Updates automatically':
+		'Actualitzacions automàtiques',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Instal·leu i no us amoïneu per res! L\'Unchecky s\'actualitza automàticament cada vegada que una versió nova està disponible. Per tant, sempre tindreu la versió més recent.',
+	
+	'Watch Unchecky in action':
+		'Veieu a l\'Unchecky en acció',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Opinions dels experts</a> i <a href="press#videos">més vídeos dels nostres usuaris</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Opinions dels experts',
+	'Unchecky was also featured on the following international websites:':
+		'L\'Unchecky és conegut arreu del planeta:',
+	'on':
+		'de',
+	'Awards':
+		'Guardons',
+	
+	'People are showing Unchecky in action':
+		'Vídeos dels nostres usuaris',
+	'Demonstrating Unchecky':
+		'L\'Unchecky en acció',
+	
+	'German':
+		'Alemany',
+	'English':
+		'Anglès',
+	'Bulgarian':
+		'Búlgar',
+	'Catalan':
+		'Català',
+	'Danish':
+		'Danès',
+	'Slovenian':
+		'Eslovè',
+	'Spanish':
+		'Espanyol',
+	'French':
+		'Francès',
+	'Italian':
+		'Italià',
+	'Japanese':
+		'Japonès',
+	'Dutch':
+		'Neerlandès',
+	'Norwegian':
+		'Noruec',
+	'Polish':
+		'Polonès',
+	'Portuguese':
+		'Portuguès',
+	'Russian':
+		'Rus',
+	'Swedish':
+		'Suec',
+	'Turkish':
+		'Turc',
+	'Chinese':
+		'Xinès',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Què necessito per utilitzar l\'Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Podeu instal·lar l\'Unchecky en Windows XP o versions més recents.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'El meu antivirus detecta l\'Unchecky com a aplicació potencialment perillosa.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">was found 100% clean</a>.':
+		'És un fals positiu. Informeu a l\'empresa del vostre antivirus d\'aquest comportament. L\'Unchecky està certificat per Softpedia com a <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">aplicació 100% segura</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Quina és la millor manera d\'avisar d\'un error o fer un suggeriment?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Podeu publicar (en anglès) el vostre comentari al <a href="http://unchecky.userecho.com/">web de l\'UserEcho</a>.',
+	
+	'I want to translate Unchecky.':
+		'M\'agradaria traduir l\'Unchecky',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Els fitxers de traducció els trobareu al nostre repositori del <a href="https://github.com/RaMMicHaeL/unchecky-translate">GitHub</a>',
+	
+	'Is it possible to install Unchecky silently?':
+		'Es pot instal·lar l\'Unchecky de forma silenciosa?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Oi tant! Inicieu l\'instal·lador amb <code>-install</code> a la línia d\'ordres. Si voleu conèixer totes les variables, llavors executeu <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Si us plau, NO utilitzeu aquest formulari per informar d\'errors o fer suggeriments. Us recordem que per aquesta finalitat heu d\'adreçar-vos al <a href="http://unchecky.userecho.com/">web de l\'UserEcho</a>. El missatge ha d\'estar redactat només en anglès',
+	
+	'First Name':
+		'Nom',
+	'Last Name':
+		'Cognoms',
+	'Email Address':
+		'Adreça electrònica',
+	'Subject':
+		'Assumpte',
+	'Message':
+		'Missatge',
+	'Send':
+		'Envia',
+	
+	'Please fill in all fields.':
+		'És obligatori omplir tots els camps.',
+	'Please enter a valid email address.':
+		'Inseriu una adreça de correu electrònic vàlida',
+	'Could not complete your request.':
+		'No s\'ha pogut dur a terme la petició.',
+	'The message has been sent successfully.':
+		'El missatge s\'ha enviat correctament',
+	
+	// 404
+	
+	'Not found':
+		'No s\'ha trobat',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'La pàgina que esteu cercant no existeix.',
+	
+}
