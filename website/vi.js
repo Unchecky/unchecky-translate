33af972f4ab2e3378ccecb71acff6a85cb705853
Nguyễn Thanh Tài <thanhtai2009@afublog.com>
2015-02-19 12:49:04 +0700

    Added Vietnamese translation for website

---
+++ website/vi.js
@@ -0,0 +1,163 @@
+jquery_lang_js.prototype.lang.vi = {
+	
+	// Header
+	
+	'Home':
+		'Trang chủ',
+	'Press':
+		'Báo chí',
+	'FAQ':
+		'Câu hỏi thường gặp',
+	'Changelog':
+		'Nhật ký thay đổi',
+	'Contact':
+		'Liên hệ',
+	'Links':
+		'Liên kết',
+	'Bug/feature tracker':
+		'Theo dõi lỗi/Tính năng',
+	'Blog':
+		'Blog',
+	'Language':
+		'Ngôn ngữ',
+	
+	// Footer
+	
+	'Copyright © 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Bản quyền © 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>. Bản dịch tiếng Việt: <a href="mailto:thanhtai2009@outlook.com?Subject=Unchecky">thanhtai2009@tekcafe.vn</a>, Nguyễn Thanh Tài.',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Thoả thuận cấp phép với người dùng cuối',
+	'Privacy Policy':
+		'Chính sách riêng tư',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Mệt mỏi vì những đề nghị không liên qua khi cài đặt phần mềm?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Bạn đã bảo giờ cảm thấy khi cài đặt phần mềm, nó cố gắng bằng mọi cách cài thêm những chương trình khác? Mỗi khi bỏ quên một mục chọn và phải bỏ ra hàng giờ sau đó để gỡ cài đặt phần mềm quảng cáo? Khi mở trình duyệt sau khi cài đặt, chỉ phát hiện ra một trang chủ mới, một công cụ tìm kiếm mới hoặc ngay cả một trình duyệt mới?<br>Unchecky được thiết kế nhằm giúp bạn tránh khỏi những chương trình không mong muốn xâm nhập máy tính.',
+	'Download':
+		'Tải xuống',
+	
+	'Introducing Unchecky!':
+		'Giới thiệu Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Lựa chọn sáng suốt.',
+	
+	'Unchecks':
+		'Bỏ chọn',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky tự động bỏ chọn những đề nghị không liên quan, giúp bạn click chuột ít hơn và giúp bạn ít bỏ lỡ những đề nghị không mong muốn hơn.',
+	
+	'Warns':
+		'Cảnh báo',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Những trình cài đặt cốc gắng cài thêm chương trình bổ sung như một phần của quá trình cài đặt. Unchecky cảnh báo bạn khi bạn cố chấp nhận một đề nghị không mong muốn, giúp giảm khả năng chấp nhận nhầm.',
+	
+	'Updates automatically':
+		'Tự động cập nhật',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Cài đạt và quên đi. Unchecky sẽ tự động cập nhật khi có phiên bản mới, vậy nên bạn không cần lo lắng về việc có dùng phiên bản mới nhất hay không.',
+	
+	'Watch Unchecky in action':
+		'Xem cách Unchecky hoạt động',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Xem những gì người khác nói về Unchecky sagen</a> hoặc <a href="press#videos">xem thêm video</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Mọi người nói về Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky cũng hiện diện trên những website quốc tế sau:',
+	'on':
+		'trên',
+	'Awards':
+		'Giải thưởng:',
+	
+	'People are showing Unchecky in action':
+		'Mọi người trình diễn Unchecky',
+	'Demonstrating Unchecky':
+		'Trình diễn Unchecky',
+	
+	'Portuguese':
+		'portugiesisch',
+	'Polish':
+		'polnisch',
+	'Russian':
+		'russisch',
+	'Spanish':
+		'spanisch',
+	'Italian':
+		'italienisch',
+	'French':
+		'französisch',
+	'German':
+		'deutsch',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Cấu hình yêu cầu để sử dụng Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky hỗ trợ Windows XP và mới hơn.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Trình chống virus cho Unchecky là nguy hiểm.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">was found 100% clean</a>.':
+		'Đó là phát hiện nhầm. Vui lòng báo cáo về nó đến công ty phần mềm chống virus bạn đang dùng. Hãy nhớ rằng Unchecky đã được kiểm định kỹ lưỡng bởi Softpedia và <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">an toàn 100%</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Đâu là cách báo cáo lỗi hoặc đề nghị tính năng tốt nhất?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Viết phản hồi của bạn tại <a href="http://unchecky.userecho.com/">trang UserEcho</a>.',
+	
+	'I want to translate Unchecky.':
+		'Tôi muốn dịch Unchecky!',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Những tập tin dịch thuật của chương trình và website Unchecky có thể tìm thấy tại <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>- kho lưu trữ GitHub.',
+	
+	'Is it possible to install Unchecky silently?':
+		'Có thể cài đặt Unchecky trong thầm lặng chứ?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Dĩ nhiên, hãy chạy trình cài đặt với dòng lệnh <code>-install</code>. Để có danh sách những lệnh được hỗ trợ, hãy chạy <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Xin đừng dùng mẫu liên hệ này để báo cáo lỗi hoặc gợi ý tính năng mới. Hãy viết phản hồi của bạn tại <a href="http://unchecky.userecho.com/">trang UserEcho</a>.',
+	
+	'First Name':
+		'Tên',
+	'Last Name':
+		'Họ',
+	'Email Address':
+		'Địa chỉ email',
+	'Subject':
+		'Tiêu đề',
+	'Message':
+		'Nội dung',
+	'Send':
+		'Gửi',
+		
+	'Please fill in all fields.':
+		'Hãy điền tất cả các trường!',
+	'Please enter a valid email address.':
+		'Hãy nhập địa chỉ email hợp lệ!',
+	'Could not complete your request.':
+		'Không thể hoàn tất yêu cầu của bạn.',
+	'The message has been sent successfully.':
+		'Tin nhắn đã được gửi thành công!',
+	
+	// 404
+	
+	'Not found':
+		'Không tìm thấy nội dung',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Xin lỗi, trang bạn đang tìm không khả dụng.',
+	
+}
