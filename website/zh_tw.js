c7047ec3a6ca71e96a4c3f4368a88fb5f904ed54
RaMMicHaeL <mmm.maltsev@gmail.com>
2015-04-05 12:38:59 +0300

    Added Chinese (Traditional, Taiwan) website translation

---
+++ website/zh_tw.js
@@ -0,0 +1,163 @@
+jquery_lang_js.prototype.lang.zh_tw = {
+	
+	// Header
+	
+	'Home':
+		'首頁',
+	'Press':
+		'新聞',
+	'FAQ':
+		'常見問題',
+	'Changelog':
+		'更新日誌',
+	'Contact':
+		'聯繫',
+	'Links':
+		'連結',
+	'Bug/feature tracker':
+		'錯誤/功能追蹤',
+	'Blog':
+		'部落格',
+	'Language':
+		'語言',
+	
+	// Footer
+	
+	'Copyright © 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'版權所有 c 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>。 由 <a href="https://www.facebook.com/ChiaHong.H">洪嘉鴻 翻譯</a>。',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'最終用戶許可協議',
+	'Privacy Policy':
+		'隱私權政策',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'您已厭倦在安裝軟體時一直要取消勾選無關的程式了嗎?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'您有沒有覺得，在安裝軟體時，安裝程式試圖推送更多的非必要程式而不惜一切代價？ 曾經錯過了一個核取方塊，之後即要花費數小時移除廣告軟體？ 曾經在安裝程式後開啟瀏覽器，卻發現您有了新的首頁、新的搜尋引擎，甚至是新的瀏覽器？<br>Unchecky 為一款讓可能非必要的程式不侵害您電腦的軟體。',
+	'Download':
+		'下載',
+	
+	'Introducing Unchecky!':
+		'Unchecky 介紹!',
+	'Keeps your checkboxes clear.':
+		'讓您的核取方塊保持取消選擇。',
+	
+	'Unchecks':
+		'取消勾選',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky 自動取消不相關的選擇，使您不需一直點擊來取消無用的選項，以減少錯過推送非必要資訊的核取方塊。',
+	
+	'Warns':
+		'警告',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'安裝程式往往試圖加入附加程式作為正常安裝的一個部分。 Unchecky 會在當您嘗試接受一個非必要程式時提醒您，這使得它不太可能被意外地接受。',
+	
+	'Updates automatically':
+		'自動更新',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'忘記安裝新版本。每當有新版本發布時，Unchecky 會自動更新，所以您不必擔心執行的是否為最新程式。',
+	
+	'Watch Unchecky in action':
+		'觀看 Unchecky 執行的動作',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">看看大家對於 Unchecky 說些什麼</a> 或 <a href="press#videos">觀看更多的影片</a>。',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'大家正在談論 Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky 也在下列國際網站上被描述：',
+	'on':
+		'在',
+	'Awards':
+		'獎項',
+	
+	'People are showing Unchecky in action':
+		'大家在展示 Unchecky 的動作',
+	'Demonstrating Unchecky':
+		'展示 Unchecky',
+	
+	'Portuguese':
+		'葡萄牙語',
+	'Polish':
+		'波蘭語',
+	'Russian':
+		'俄語',
+	'Spanish':
+		'西班牙語',
+	'Italian':
+		'義大利語',
+	'French':
+		'法語',
+	'German':
+		'德語',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Unchecky 的系統要求是什麼？',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky 支援 Windows XP 和更新的作業系統。',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'我的防毒軟體認定 Unchecky 為惡意軟體。',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">was found 100% clean</a>.':
+		'其為誤報。 請向您的防毒軟體公司報告。 請您注意，Unchecky 已由 <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">檢測完成，是 100% 安全的</a>。',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'報告錯誤或提出功能建議的最好的方法是什麼？',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'請發表您的回饋於 <a href="http://unchecky.userecho.com/">使用者回饋頁面</a>。',
+	
+	'I want to translate Unchecky.':
+		'我想要翻譯 Unchecky。',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Unchecky 程式和網站的翻譯檔案可於 <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-翻譯</a>GitHub 資料庫中找到。',
+	
+	'Is it possible to install Unchecky silently?':
+		'是否可能以背景安裝 Unchecky?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'可以，請以 <code>-install</code> 命令列執行安裝程式。 對於支援的命令表，請執行 <code>unchecky_setup.exe -?</code>。',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'請不要使用此聯繫表格來報告錯誤或提出功能建議。 請於 <a href="http://unchecky.userecho.com/">使用者回饋頁面</a>發表您的回饋。',
+	
+	'First Name':
+		'名字',
+	'Last Name':
+		'姓氏',
+	'Email Address':
+		'電子郵件地址',
+	'Subject':
+		'主旨',
+	'Message':
+		'訊息',
+	'Send':
+		'傳送',
+		
+	'Please fill in all fields.':
+		'請填寫所有的項目。',
+	'Please enter a valid email address.':
+		'請輸入有效的電子郵件地址。',
+	'Could not complete your request.':
+		'不能完成您的請求。',
+	'The message has been sent successfully.':
+		'訊息已成功傳送。',
+	
+	// 404
+	
+	'Not found':
+		'未找到',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'很抱歉，找不到您要尋找的頁面。',
+	
+}
