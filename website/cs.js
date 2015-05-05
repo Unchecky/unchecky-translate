8730f4f120633b00409b0960190a81b8c9503951
RaMMicHaeL <mmm.maltsev@gmail.com>
2015-05-05 18:05:15 +0300

    Added Czech website translation

---
+++ website/cs.js
@@ -0,0 +1,170 @@
+jquery_lang_js.prototype.lang.cs = {
+	
+	// Header
+	
+	'Home':
+		'Domů',
+	'Press':
+		'Z tisku',
+	'FAQ':
+		'FAQ',
+	'Changelog':
+		'Seznam změn',
+	'Contact':
+		'Kontakt',
+	'Links':
+		'Odkazy',
+	'Bug/feature tracker':
+		'Hlášení chyb / vylepšení',
+	'Blog':
+		'Blog',
+	'Language':
+		'Jazyk',
+	
+	// Footer
+	
+	'Copyright © 2015 Reason Company Software Inc.':
+		'Copyright © 2015 Reason Company Software Inc. Český překlad: <a href="mailto:lukkemarten@gmail.com?Subject=Unchecky">Lukke213</a>.',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Licenční ujednání',
+	'Privacy Policy':
+		'Ochrana osobních údajů',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Jste unaveni z odznačování nabídek během instalace programu?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Zažili jste již, že zatímco instalujete program, instalátor se Vám snaží neustále nutit nějakou nabídku? Nebo jste zapomněli odznačit nabídku a poté jste strávili hodiny odstraňováním nevyžádané aplikace? A nebo se Vám otevřel po instalaci programu internetový prohlížeč a v něm nastavena nová domovská stránka, či ještě hůře nový internetový prohlížeč?<br>Unchecky pomáha držet potenciálně nechtěné aplikace od vašeho počítače.',
+	'Download':
+		'Stáhnout',
+	
+	'Introducing Unchecky!':
+		'Přivítejte Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Již žádné zaznačené nabídky.',
+	
+	'Unchecks':
+		'Odznačení',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky automaticky odznačí nežádoucí nabídku, ušetří mnoho kliknutí myší a zamezí tak i možnosti, že se při odznačování nabídky překliknete.',
+	
+	'Warns':
+		'Warnt dich',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Instalátory se často pokouší nainstalovat nepotřebné aplikace tvářící se jako součást instalace. Unchecky Vás upozorní na to, že se pokoušíte schválit instalaci potenciálně nechtěné aplikace, kterou by jste normálně ani nezaznamenali.',
+	
+	'Updates automatically':
+		'Automatická aktualizace',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Nainstalovat a zapomenout. Unchecky se aktualizuje automaticky a vy se tak již nemusíte starat o to, zda používáte poslední verzi.',
+	
+	'Watch Unchecky in action':
+		'Unchecky v akci',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Podívejte se co lidé říkají o Unchecky</a> nebo můžete <a href="press#videos">shlédnout více videí</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Co lidé říkají o Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky rovněž hraje hlavní roli na následujících mezinárodních webových stránkách:',
+	'on':
+		'na',
+	'Awards':
+		'Ocenění:',
+	
+	'People are showing Unchecky in action':
+		'Unchecky v akci',
+	'Demonstrating Unchecky':
+		'Představení Unchecky',
+	
+	'Portuguese':
+		'portugalština',
+	'Polish':
+		'polština',
+	'Russian':
+		'ruština',
+	'Spanish':
+		'španelština',
+	'Italian':
+		'italština',
+	'French':
+		'francouzština',
+	'German':
+		'němčina',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Jaké jsou systémové požadavky pro Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky podporuje operační systém Windows XP a vyšší.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Můj antivir označuje Unchecky za škodlivý.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">was found 100% clean</a>.':
+		'Jedná se o falešný poplach. Nahlašte prosím tuto událost ve Vašem antivirovém programu. Unchecky byl důkladně testován společností Softpedia a <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">byl 100% čistý</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Kde mám správně hlásit chyby či náměty k vylepšení?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Pište Vaše reakce na <a href="http://unchecky.userecho.com/">UserEcho stránku</a>.',
+	
+	'How do I uninstall Unchecky?':
+		'Jak odinstalovat Unchecky?',
+	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
+		'Unchecky můžete odinstalovat v Ovládacích panelech, tak jako ostatní programy. Instrukce pro <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a> nebo <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.',
+	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
+		'Pokud by běžný postup odinstalování selhal, zkuste prosím použít aplikaci <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, která je určena pro odebrání poškozené instalace Unchecky.',
+	
+	'I want to translate Unchecky.':
+		'Chci přeložit Unchecky!',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Soubory pro překlad programu Unchecky a webových stránek naleznete na <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> uložišti GitHub.',
+	
+	'Is it possible to install Unchecky silently?':
+		'Je možné nainstalovat Unchecky pomocí tichého instalátoru?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Ano, stačí spustit instalaci s parametrem <code>-install</code> v příkazovém řádku. Seznam všech podporovaných příkazů zobrazíte zadáním parametru <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Nepoužívejte prosím tento formulář pro hlášení chyb či vylepšení.<br>Vaše reakce pište do <a href="http://unchecky.userecho.com/">UserEcho stránky</a>.',
+	
+	'First Name':
+		'Jméno',
+	'Last Name':
+		'Příjmení',
+	'Email Address':
+		'E-mail',
+	'Subject':
+		'Předmět',
+	'Message':
+		'Vzkaz',
+	'Send':
+		'Odeslat',
+		
+	'Please fill in all fields.':
+		'Vyplňte prosím všechna pole!',
+	'Please enter a valid email address.':
+		'Zadejte prosím platnou emailovou adresu!',
+	'Could not complete your request.':
+		'Váš požadavek nelze dokončit.',
+	'The message has been sent successfully.':
+		'Vzkaz byl úspěšně odeslán!',
+	
+	// 404
+	
+	'Not found':
+		'Nenalezeno',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Omlouváme se, ale stránka kterou hledáte nebyla nalezena.',
+	
+}
