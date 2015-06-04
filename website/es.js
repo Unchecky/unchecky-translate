e6570aada4c22eb43622a96e010dc73516ba069e
RaMMicHaeL <mmm.maltsev@gmail.com>
2015-06-04 16:18:14 +0300

    Added website Spanish translation

---
+++ website/es.js
@@ -0,0 +1,192 @@
+jquery_lang_js.prototype.lang.es = {
+	
+	// Header
+	
+	'Home':
+		'Inicio',
+	'Press':
+		'Prensa',
+	'FAQ':
+		'FAQ',
+	'Changelog':
+		'Notas de la versión',
+	'Contact':
+		'Contacto',
+	'Links':
+		'Enlaces',
+	'Bug/feature tracker':
+		'Errores y sugerencias',
+	'Blog':
+		'Blog',
+	'Language':
+		'Idioma',
+	
+	// Footer
+	
+	'Copyright © 2015 Reason Company Software Inc.':
+		'© 2015 Reason Company Software Inc. Traducción a cargo de: <a href="mailto:adruk.i@gmail.com">adruki</a>',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Acuerdo de licencia de usuario final',
+	'Privacy Policy':
+		'Política de privacidad',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'¿Hartos de desmarcar ofertas irrelevantes al instalar software?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'¿No os ha pasado alguna vez que el instalador de una aplicación os ofrece software que nada tiene que ver con el que estáis instalando? ¿Habéis perdido horas eliminando basura del equipo "gracias" a las casillas ininteligibles que venían marcadas por defecto? Esto para no hablar del secuestro de la página de inicio, cambios del motor de búsqueda, o incluso la instalación de un nuevo navegador que se convierta en el navegador por defecto... Unchecky es una utilidad que tiene como objetivo evitar la instalación de aplicaciones potencialmente peligrosas al ordenador.',
+	'Download':
+		'Descarga gratuita',
+	
+	'Introducing Unchecky!':
+		'¡Os presentamos Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Mantiene a raya las instalaciones no deseadas.',
+	
+	'Unchecks':
+		'Desmarca',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky inhabilita automáticamente las ofertas no relacionadas, ahorrando clics del ratón y lo más importante; protegiéndolo contra aplicaciones potencialmente peligrosas.',
+	
+	'Warns':
+		'Avisa',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Los instaladores a menudo intentan añadir software adicional como una parte natural de la instalación. Unchecky os avisa al encontrar una oferta potencialmente no deseada, minimizando la posibilidad de que sean aceptadas por accidente.',
+	
+	'Updates automatically':
+		'Actualizaciones automáticas',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'¡Instaladlo y olvidaos! Unchecky se actualiza automáticamente cada vez que una nueva versión está disponible. Por lo tanto, siempre tendréis la versión más reciente.',
+	
+	'Watch Unchecky in action':
+		'Mirad a Unchecky en acción',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Opiniones de los expertos</a> y <a href="press#videos">más vídeos de nuestros usuarios</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Opiniones de los expertos',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky es conocido en todo el mundo:',
+	'on':
+		'de',
+	'Awards':
+		'Galardones',
+	
+	'People are showing Unchecky in action':
+		'Vídeos de nuestros usuarios',
+	'Demonstrating Unchecky':
+		'Unchecky en acción',
+	
+	'German':
+		'Aleman',
+	'English':
+		'Inglés',
+	'Bulgarian':
+		'Búlgaro',
+	'Catalan':
+		'Catalán',
+	'Danish':
+		'Danés',
+	'Slovenian':
+		'Esloveno',
+	'Spanish':
+		'Español',
+	'French':
+		'Francés',
+	'Italian':
+		'Italiano',
+	'Japanese':
+		'Japonés',
+	'Dutch':
+		'Neerlandés',
+	'Norwegian':
+		'Noruego',
+	'Polish':
+		'Polaco',
+	'Portuguese':
+		'Portugués',
+	'Russian':
+		'Ruso',
+	'Swedish':
+		'Sueco',
+	'Turkish':
+		'Turco',
+	'Chinese':
+		'Chino',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'¿Cúales  son los requisitos para utilizar Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Podéis instalar Unchecky en Windows XP o versiones más recientes.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Mi antivirus detecta Unchecky como aplicación potencialmente peligrosa.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">was found 100% clean</a>.':
+		'Es un falso positivo. Informáis a la empresa de vuestro antivirus de este comportamiento. Unchecky está certificado por Softpedia como <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">aplicación 100% segura</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'¿Cuál es la mejor manera de avisar de un error o hacer una sugerencia?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Podéis publicar (en inglés) vuestro comentario en la <a href="http://unchecky.userecho.com/">web de UserEcho</a>.',
+	
+	'How do I uninstall Unchecky?':
+		'¿Como desinstalo Unchecky?',
+	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
+		'Unchecky puede ser desinstalado desde el Panel de Control, como cualquier otro programa. Puedes encontrar instrucciones para <a href="http://windows.microsoft.com/es-es/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/es-es/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/es-es/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, o <a href="https://support.microsoft.com/es-es/kb/307895">Windows XP</a>.',
+	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
+		'Si el método estándar falla, puedes utilizar <a href="files/unchecky_cleaner.exe">Unchecky CleanerTool</a>, una herramienta diseñada para desinstalar instalaciones de Unchecky corruptas.',
+	
+	'I want to translate Unchecky.':
+		'Me gustaría traducir Unchecky',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Los ficheros de traducción los encontraréis en nuestro <a href="https://github.com/RaMMicHaeL/unchecky-translate">repositorio de GitHub</a>',
+	
+	'Is it possible to install Unchecky silently?':
+		'¿Se puede instalar Unchecky de forma silenciosa?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'¡Por supuesto! Iniciad el instalador con <code>-install</code> en la línea de comandos. Si queréis conocer todas las variables, entonces ejecutad <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Por favor, NO utilicéis este formulario para informar de errores o hacer sugerencias. Os recordamos que para este fin tenéis que dirigiros a la <a href="http://unchecky.userecho.com/">web de UserEcho</a>. El mensaje tiene que estar redactado únicamente en inglés.',
+	
+	'First Name':
+		'Nombre',
+	'Last Name':
+		'Apellidos',
+	'Email Address':
+		'Dirección electrónica',
+	'Subject':
+		'Asunto',
+	'Message':
+		'Mensaje',
+	'Send':
+		'enviar',
+	
+	'Please fill in all fields.':
+		'Es obligatorio rellenar todos los campos.',
+	'Please enter a valid email address.':
+		'Inserte una dirección de correo electrónico válida',
+	'Could not complete your request.':
+		'No se ha podido llevar a cabo la petición.',
+	'The message has been sent successfully.':
+		'El mensaje se ha enviado correctamente',
+	
+	// 404
+	
+	'Not found':
+		'No encontrado',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'La página que estas buscando no existe.',
+	
+}
