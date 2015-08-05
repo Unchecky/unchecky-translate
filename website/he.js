cc14b835715967dd3c72361d7eeaf65ba7d4face
RaMMicHaeL <mmm.maltsev@gmail.com>
2015-08-05 14:24:50 +0300

    Added Hebrew website translation

---
+++ website/he.js
@@ -0,0 +1,170 @@
+jquery_lang_js.prototype.lang.he = {
+	
+	// Header
+	
+	'Home':
+		'בית',
+	'Press':
+		'עיתונות',
+	'FAQ':
+		'שאלות נפוצות',
+	'Changelog':
+		'רשימת שינויים',
+	'Contact':
+		'צור קשר',
+	'Links':
+		'קישורים',
+	'Bug/feature tracker':
+		'Bug/feature tracker',
+	'Blog':
+		'בלוג',
+	'Language':
+		'שפה',
+	
+	// Footer
+	
+	'Copyright © 2015 Reason Company Software Inc.':
+		'Copyright © 2015 Reason Company Software Inc.',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'הסכם הרשאה למשתמש - קצה',
+	'Privacy Policy':
+		'מדיניות פרטיות',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'נמאס לך לסמן הצעות תוכנה לא קשורות בזמן התקנה?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'האם הרגשת, במהלך התקנה, שמנסים לשווק לך תוכנות לא רצויות בכל מחיר? האם פספסת אי פעם תיבות ולאחר מכן בילית שעות בהסרת תוכנות מזיקות?',
+	'Download':
+		'הורדה',
+	
+	'Introducing Unchecky!':
+		'הכירו את אנצ׳קי!',
+	'Keeps your checkboxes clear.':
+		'שומר על תיבות הסימון (צ׳קבוקס) שלך נקיות!',
+	
+	'Unchecks':
+		'מנקה סימונים',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'אנצ׳קי מנקה באופן עצמאי את תיבות הסימון (צ׳קבוקס) שגורמים להתקנת תוכנות בילתי רצויות במהלך ההתקנה וחוסך לך הקלקות עכבר מרובות וטעויות מצערות',
+	
+	'Warns':
+		'מזהיר',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'באורח קבע, תוכנות התקנה מנסות ״להגניב״ למשתמש תוכנות נוספות כחלק מתהליך ההתקנה. אנצ׳קי מזהיר אותך כאשר זה קורה ובכך מקטין את הסיכוי להתקנות לא רצויות.',
+	
+	'Updates automatically':
+		'מתעדכן באופן עצמאי',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'להתקין ולשכוח. אנצ׳קי מתעדכן באופן עצמאי בכל פעם שאנחנו מוציאים גרסא חדשה, כך שלא צריך לדאוג לעדכון אנצ׳קי לגרסתו האחרונה לבד',
+	
+	'Watch Unchecky in action':
+		'צפו באנצ׳קי בפעולה',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">מה המשתמשים אומרים על אנצ׳קי?</a> וגם <a href="press#videos">צפו בקטעי וידאו נוספים</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'אנשים מדברים מדברים על אנצ׳קי',
+	'Unchecky was also featured on the following international websites:':
+		':אנצ׳קי גם מוזכר בכתבות ברחבי האינטרנט',
+	'on':
+		'דרך',
+	'Awards':
+		'פרסים',
+	
+	'People are showing Unchecky in action':
+		'אנשים מציגים את אנצ׳קי בפעולה',
+	'Demonstrating Unchecky':
+		'הדגמות של אנצ׳קי',
+	
+	'Portuguese':
+		'פורטוגזית',
+	'Polish':
+		'פולנית',
+	'Russian':
+		'רוסית',
+	'Spanish':
+		'ספרדית',
+	'Italian':
+		'איטלקית',
+	'French':
+		'צרפתית',
+	'German':
+		'גרמנית',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'מהן דרישות המערכת של אנצ׳קי?',
+	'Unchecky supports Windows XP and above.':
+		'אנצ׳קי תומך בגרסת Windows XP ומעלה.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'האנטי וירוס שלי מסמן את אנצ׳קי כתוכנה זדונית.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">was found 100% clean</a>.':
+		'זה דיווח שווא. אנא דווחו לחברת האנטי וירוס שלכם. שימו לב כי אנצ׳קי נבדק ביסודיות על ידי סופטפדיה ונמצא <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">נקי ב-100%</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'מה הדרך הטובה ביותר לדווח על תקלות או לבקש הרחבות לאנצ׳קי?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'לשליחת היזון חוזר הכנסו לדף ה-<a href="http://unchecky.userecho.com/">UserEcho</a>.',
+	
+	'How do I uninstall Unchecky?':
+		'כיצד להסיר את אנצ׳קי?',
+	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
+		'בדומה לכל תוכנה אחרת גם אנצ׳קי ניתנת להסרה מלוח הבקרה, להנחיות נוספות: <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, או <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.',
+	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
+		'במידה וישנה בעיה, יש לנסות את <a href="files/unchecky_cleaner.exe">כלי הניקוי של אנצ׳קי</a> שמתוכנן להסרה של התקנות תקולות של אנצ׳קי.',
+	
+	'I want to translate Unchecky.':
+		'אני רוצה לתרגם את אנצ׳קי.',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'קבצי התרגום נמצאים כאן: <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.',
+	
+	'Is it possible to install Unchecky silently?':
+		'האם לאנצ׳קי יש התקנה שקטה?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'כן, יש להריץ את תוכנת ההתקנה עם המשתנה <code>-install</code>, ניתן גם להריץ <code>unchecky_setup.exe -?</code> בכדי להציג את רשימת המשתנים לתוכנת ההתקנה בשורת הפקודה.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'אנא אל תשתמשו בטופס יצירת הקשר לדיווח על שגיאות או להצעת תכונות חדשות למוצר. במקום זאת השתמשו ב <a href="http://unchecky.userecho.com/">דף UserEcho</a>.',
+	
+	'First Name':
+		'שם פרטי',
+	'Last Name':
+		'שם משפחה',
+	'Email Address':
+		'דואר אלקטרוני',
+	'Subject':
+		'נושא',
+	'Message':
+		'תוכן ההודעה',
+	'Send':
+		'שליחה',
+		
+	'Please fill in all fields.':
+		'יש למלא את כל השדות',
+	'Please enter a valid email address.':
+		'יש להכניס כתובת דואר אלקטרוני תקינה',
+	'Could not complete your request.':
+		'לא ניתן להשלים את הבקשה',
+	'The message has been sent successfully.':
+		'ההודעה נשלחה בהצלחה',
+	
+	// 404
+	
+	'Not found':
+		'לא קיים',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'אנו מצטערים אך דף זה אינו קיים',
+	
+}
