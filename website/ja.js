a224d85185e9cb5b624edf9b840908808064663d
asukanagisa <asukanagisa@gmail.com>
2015-08-16 02:34:53 +0900

    Update ja.js
    Adjustment

--- website/ja.js
+++ website/ja.js
@@ -15,7 +15,7 @@
 	'Links':
 		'リンク',
 	'Bug/feature tracker':
-		'不具合/機能改善に関する報告',
+		'不具合/改善提案の報告',
 	'Blog':
 		'ブログ',
 	'Language':
@@ -36,31 +36,31 @@
 	// Home
 	
 	'Tired of unchecking unrelated offers while installing software?':
-		'ソフトウェアのインストール中、関連性のないオファーのチェックを外すのはうんざりですか？',
+		'ソフトのインストール中、無関係なチェックを外すのにうんざりしませんか？',
 	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
-		'ソフトウェアのインストール中に不要なプログラムのチェックを外してインストールすることを煩わしく感じたことはありませんか？ またチェックを外し忘れ、その後にアドウェアを削除する時間を要したことはありませんか？さらにプログラムをインストール後、ブラウザを開いた際にホーム設定等が改ざんされたということはありませんか？<br>Uncheckyはお使いのコンピューターに、不要なプログラムを導入させないことを目指しています。',
+		'ソフトウェアのインストール中、付いてくる不要なソフトのチェックを外してインストールするのを煩わしく感じたことはありませんか？ チェックを外し忘れ、あとからアドウェアの削除に時間はかかりませんでしたか？ソフトウェアのインストール後、ブラウザを開いたらホームページ等が変えられていたことはありませんか？<br>Uncheckyはお使いのコンピューターに、不要なソフトウェアを導入させないことを目指しています。',
 	'Download':
 		'ダウンロード',
 	
 	'Introducing Unchecky!':
 		'Uncheckyのご紹介！',
 	'Keeps your checkboxes clear.':
-		'チェックボックスを除去します。',
+		'チェックボックスをクリアに保ちます。',
 	
 	'Unchecks':
 		'アンチェック',
 	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
-		'Uncheckyは自動的に関連性のないオファーのチェックを外すため、あなたがマウスをクリックする手間を省き、さらにチェックを外し忘れるというリスクを軽減することが出来ます。',
+		'Uncheckyは無関係なソフトのチェックを自動的に外すので、あなたがマウスをクリックする手間を省き、さらにチェックを外し忘れる可能性も減らします。',
 	
 	'Warns':
 		'警告の表示',
 	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
-		'インストーラーは多くの場合、自然な形で追加プログラムのインストールを行います。Uncheckyはあなたが不要なオファーを許可しようとした際に警告を表示し、誤って不要なオファーを受け入れてしまうリスクを軽減させます。',
+		'インストーラーは多くの場合、自然な流れに見せてこっそり追加ソフトをインストールしようとします。Uncheckyはあなたが無関係なソフトを許可しようとした際に警告を表示し、誤ってインストールされる可能性を減らします。',
 	
 	'Updates automatically':
 		'自動更新',
 	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
-		'新しいバージョンのインストールを忘れてしまった。しかしUncheckyは新しいバージョンが公開された際に自動的に更新されるため、いつでも最新の状態を保つことが出来るので心配はいりません。',
+		'インストールしたら忘れましょう。Uncheckyは新しいバージョンが公開されると自動更新され、いつも最新の状態に保たれるので心配はいりません。',
 	
 	'Watch Unchecky in action':
 		'動画でUncheckyを見る',
@@ -74,14 +74,14 @@
 	'Unchecky was also featured on the following international websites:':
 		'Uncheckyは以下のウェブサイト上で紹介されました：',
 	'on':
-		'上',
+		'：',
 	'Awards':
 		'アワード',
 	
 	'People are showing Unchecky in action':
 		'動作中のUncheckyを表示します',
 	'Demonstrating Unchecky':
-		'Uncheckyの実証',
+		'Uncheckyのデモンストレーション',
 	
 	'Portuguese':
 		'ポルトガル語',
@@ -106,36 +106,36 @@
 		'UncheckyはWindows XP及び以後のバージョンをサポートしています。',
 	
 	'My antivirus flags Unchecky as malicious.':
-		'セキュリティソフトにより、Uncheckyがマルウェアとして検出されます。',
+		'セキュリティソフトにより、Uncheckyが危険と検出されます。',
 	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">was found 100% clean</a>.':
-		'それは誤検出です。セキュリティベンダーに誤検出に関する報告を行ってください。UncheckyはSoftpediaにより徹底的にテストされ、その結果が示されています。<a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">100% 安全</a>',
+		'それは誤検出です。セキュリティソフトの開発元に誤検出を報告してください。UncheckyはSoftpediaにより徹底的にテストされ、その結果が<a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">100% 安全</a>であると示されています。',
 	
 	'What is the best way to report a bug or suggest a feature?':
 		'バグの報告や機能を提案するための最良の方法は何ですか？',
 	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
-		'あなたのご意見を投稿してください。<a href="http://unchecky.userecho.com/">ユーザーフォーラム</a>',
+		'あなたのご意見を<a href="http://unchecky.userecho.com/">ユーザーフォーラム（英語）</a>に投稿してください。',
 	
 	'How do I uninstall Unchecky?':
 		'Uncheckyはどのようにアンインストールしますか？',
 	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
-		'その他のプログラムと同様に、Windows コントロールパネルからアンインストールすることができます。各Windowsにおける操作手順は以下をご覧ください：<a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>、<a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>、<a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>、あるいは<a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.',
+		'一般的なプログラムと同様に、コントロールパネルからアンインストールできます。各Windowsにおける操作手順は以下をご覧ください：<a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>、<a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>、<a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>、あるいは<a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.',
 	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
 		'万一、上記のアンインストール方法で削除が出来ない場合、 <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a> を使用して下さい。',
 	
 	'I want to translate Unchecky.':
 		'Uncheckyを翻訳したい。',
 	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
-		'Uncheckyのプログラムとウェブサイトの翻訳ファイルはGitHubのリポジトリに掲載されています：<a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>',
+		'Uncheckyのプログラムとウェブサイトの翻訳ファイルはGitHubのリポジトリ<a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>に掲載されています。',
 	
 	'Is it possible to install Unchecky silently?':
 		'Uncheckyをサイレントインストールすることは可能ですか？',
 	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
-		'はい、<code>-install</code> のコマンドラインスイッチを使用してインストーラーを実行します。サポートされているコマンドラインスイッチのリストに関しては <code>unchecky_setup.exe -?</code>を実行してください。',
+		'はい、コマンドラインスイッチ<code>-install</code> をつけてインストーラーを実行します。サポートされているコマンドラインスイッチの一覧は <code>unchecky_setup.exe -?</code>を実行してください。',
 	
 	// Contact
 	
 	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
-		'バグの報告や機能改善等の提案に、このお問い合わせフォームを使用しないで下さい。左記に関するお問い合わせはユーザフォーラムに投稿してください。<a href="http://unchecky.userecho.com/">ユーザーフォーラム</a>',
+		'バグの報告や機能改善等の提案に、このお問い合わせフォームを使用しないで下さい。左記に関するお問い合わせは<a href="http://unchecky.userecho.com/">ユーザーフォーラム</a>に投稿してください。',
 	
 	'First Name':
 		'名',
