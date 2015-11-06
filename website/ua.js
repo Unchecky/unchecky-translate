7ac66ae877d4165bd7ca23bf17e19e276d426195
sawka6630 <sawka6630@users.noreply.github.com>
2015-11-06 23:33:03 +0200

    Create ua.js

---
+++ website/ua.js
@@ -0,0 +1,163 @@
+jquery_lang_js.prototype.lang.ua = {
+	
+	// Header
+	
+	'Home':
+		'Головна',
+	'Press':
+		'Преса',
+	'FAQ':
+		'ЧаПи',
+	'Changelog':
+		'Список оновлень',
+	'Contact':
+		'Зворотній зв\'язок',
+	'Links':
+		'Посилання',
+	'Bug/feature tracker':
+		'Помилки та побажання',
+	'Blog':
+		'Блог',
+	'Language':
+		'Мова',
+	
+	// Footer
+	
+	'Copyright © 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Copyright © 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>. Переклад: Кравцов Олександр.',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Ліцензійна угода',
+	'Privacy Policy':
+		'Політика конфіденційності',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Втомилися знімати галочки з пропозицій встановлення небажаного програмного забезпечення?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Коли-небудь відчували, що майстер встановлення програм намагається будь-якою ціною встановити додаткові небажані програми? Коли-небудь пропускали галочку, а потім годинами видаляли небажане програмне забезпечення? Коли-небудь відкривали свій браузер і неочікувано виявляли, що у вас змінилася домашня сторінка, пошуковий рушій або навіть увесь браузер?<br>Unchecky допоможе попередити появу потенційно небажаних програм на вашому комп’ютері.',
+	'Download':
+		'Завантажити',
+	
+	'Introducing Unchecky!':
+		'Зустрічайте Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Прибирає галки з прапорців.',
+	
+	'Unchecks':
+		'Прибирає галки за вас',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky автоматично знімає галочки сторонніх пропозицій, зберігаючи ваш час та знижуючи вірогідність встановлення небажаної програми.',
+	
+	'Warns':
+		'Попереджує',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Інсталяційні пакети часто намагаються замаскувати додаткові програми під природній процес встановлення. Unchecky попередить вас, коли ви спробуєте надати згоду на встановлення потенційно небажаної програми, що зменшить вірогідність її випадкового встановлення.',
+	
+	'Updates automatically':
+		'Автоматично оновлюється',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Встановіть та забудьте. Unchecky автоматично оновлюється щоразу, коли з\'являється оновлення, отже вам нема потреби хвилюватися про використання актуальної версії.',
+	
+	'Watch Unchecky in action':
+		'Перегляньте Unchecky в дії',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Перегляньте, що про Unchecky говорять люди</a> або <a href="press#videos">перегляньте додаткові відео</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Люди говорять про Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Програму Unchecky також було представлено на таких міжнародних вебсайтах:',
+	'on':
+		'на',
+	'Awards':
+		'Нагороди',
+	
+	'People are showing Unchecky in action':
+		'Люди демонструють Unchecky в дії',
+	'Demonstrating Unchecky':
+		'Демонстрація Unchecky',
+	
+	'Portuguese':
+		'Португальска',
+	'Polish':
+		'Польска',
+	'Russian':
+		'Російська',
+	'Spanish':
+		'Іспанська',
+	'Italian':
+		'Італійська',
+	'French':
+		'Французька',
+	'German':
+		'Німецька',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Які системні вимоги для Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky підтримує Windows XP і вище.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Мій антивірус вважає Unchecky шкідливим.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">was found 100% clean</a>.':
+		'Це хибна тривога. Будь ласка, сповістіть свою антивірусну компанію. Зверніть увагу, що Unchecky був ретельно перевірений компанією Softpedia і <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">визнаний на 100% чистим</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Як найкраще сповістити про помилку або внести пропозицію з розвитку програми?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Напишіть відгук на <a href="http://unchecky.userecho.com/">сторінці форуму UserEcho</a>.',
+	
+	'I want to translate Unchecky.':
+		'Я хочу перекласти Unchecky.',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Файли для перекладу програми Unchecky і веб-сайту можна знайти на <a href="https://github.com/RaMMicHaeL/unchecky-translate">сторінці перекладів</a> сховища GitHub.',
+	
+	'Is it possible to install Unchecky silently?':
+		'Чи можна встановити Unchecky в «тихому» режимі?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Так, запустіть інсталяційний пакет з таким параметром командного рядка: <code>-install</code>. Для перегляду повного списку параметрів командного рядка виконайте <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Будь ласка, не використовуйте цю форму для повідомлень про помилки або для пропозицій з розвитку програми. Використовуйте для цього <a href="http://unchecky.userecho.com/">сторінку форуму UserEcho</a>.',
+	
+	'First Name':
+		'Ім\'я',
+	'Last Name':
+		'Прізвище',
+	'Email Address':
+		'Адреса електронної пошти',
+	'Subject':
+		'Тема',
+	'Message':
+		'Повідомлення',
+	'Send':
+		'Надіслати',
+		
+	'Please fill in all fields.':
+		'Будь ласка, заповніть всі поля.',
+	'Please enter a valid email address.':
+		'Будь ласка, введіть дійсну адресу електронної пошти.',
+	'Could not complete your request.':
+		'Помилка при обробці вашого запиту.',
+	'The message has been sent successfully.':
+		'Повідомлення успішно надіслано.',
+	
+	// 404
+	
+	'Not found':
+		'Не знайдено',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Вибачте, але сторінки, яку ви шукали, не існує.',
+	
+}
