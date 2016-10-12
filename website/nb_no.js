00738c5dbe3653aa7ad14439022dfe2d6e703676
DandelionSprout <imreeil42@gmail.com>
2016-10-12 02:01:29 +0200

    Update nb_no.js

--- website/nb_no.js
+++ website/nb_no.js
@@ -15,7 +15,7 @@ jquery_lang_js.prototype.lang.nb_no = {
 	'Links':
 		'Lenker',
 	'Bug/feature tracker':
-		'Bug/funksjons-sporer',
+		'Bug- og funksjons-sporer',
 	'Blog':
 		'Blogg',
 	'Language':
@@ -23,18 +23,152 @@ jquery_lang_js.prototype.lang.nb_no = {
 	
 	// Footer
 	
-	'Copyright © 2016 Reason Company Software Inc.':
-		'Copyright © 2016 Reason Company Software Inc. Norsk oversettelse: <a href="mailto:alexander@feskenet.com?Subject=Unchecky">Alexander Johansen</a>',
+	'Copyright © 2015 <a href="http://rammichael.com/">RaMMicHaeL</a>.':
+		'Copyright © 2016 Reason Company Software Inc. Norsk oversettelse: <a href="mailto:alexander@feskenet.com?Subject=Unchecky">Alexander Johansen</a> og <a href="mailto:imreeil42@gmail.com?Subject=Unchecky">Imre Kristoffer Eilertsen</a>',
 	
 	// Legal
 	
 	'End-User License Agreement':
-		'',
+		'Brukerlisensavtale',
 	'Privacy Policy':
-		'',
+		'Retningslinjer for privatlivet',
 	
 	// Home
 	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Lei av å fjerne avhukinger for urelaterte tilbud når du installerer programvare?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Har du noen gang følt at når du installerer programmer, blir du forsøkt lurt til å installere tilleggsprogrammer som du ikke ønsker? Har du noen gang ved et uhell ikke fjernet en avhuking av en sjekkboks, og brukt flere timer etterpå for å fjerne reklame-innspyttende programvare? Har du noensinne åpnet nettleseren etter en installasjon og oppdaget at du har en ny startside, en ny søkemotor eller til og med en ny nettleser?<br>Unchecky forsøker å holde potensielt uønskede programmer unna din datamaskin.',
+	'Download':
+		'Last ned',
+	
+	'Introducing Unchecky!':
+		'Vi presenterer, Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Holder dine sjekkbokser rene.',
+	
+	'Unchecks':
+		'Krysser vekk',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky fjerner automatisk avkryssinger for irrelevante tilbud. Den sparer deg for flere museklikk og gjør det mindre sannsynlig å hoppe over sjekkbokser for potensielt uønskede tilbud.',
+	
+	'Warns':
+		'Advarer',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Installasjoner prøver ofte å lure inn ekstra programmer som en naturlig del av installasjonen. Unchecky advarer deg når du prøver å akseptere et potensielt uønsket tilbud, noe som gjør at det er mindre sannsynlig å akseptere ved et uhell.',
+	
+	'Updates automatically':
+		'Oppdateres automatisk',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Installer og glem alt om det. Unchecky oppdateres automatisk til enhver tid når en ny versjon er tilgjengelig, slik at du ikke trenger å bekymre deg for om du kjører den nyeste versjonen.',
+	
+	'Watch Unchecky in action':
+		'Se Unchecky i praksis',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Se hva folk sier om Unchecky</a>, eller <a href="press#videos">se flere videoer</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Det snakkes om Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky ble også fortalt om på følgende internasjonale nettsider:',
+	'on':
+		'på',
+	'Awards':
+		'Utmerkelser',
+	
+	'People are showing Unchecky in action':
+		'Folk viser Unchecky i aksjon',
+	'Demonstrating Unchecky':
+		'Demonstrasjon av Unchecky',
+	
+	'Portuguese':
+		'Portugisisk',
+	'Polish':
+		'Polsk',
+	'Russian':
+		'Russisk',
+	'Spanish':
+		'Spansk',
+	'Italian':
+		'Italiensk',
+	'French':
+		'Fransk',
+	'German':
+		'Tysk',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Hva med systemkravene for Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky støtter alle Windows-versjoner, fra og med Windows XP og nyere.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Mitt antivirusprogram sier at Unchecky er skadelig.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">was found 100% clean</a>.':
+		'Det er en falsk positiv. Vennligst rapporter dette til din antivirusprodusent. Merk at Unchecky ble nøye gjennomtestet av Softpedia og <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#clean">ble funnet 100% trygt</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Hva er den beste måten å rapportere om en bug eller foreslå nye funksjoner?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Send dine tilbakemeldinger på <a href="http://unchecky.userecho.com/">UserEcho</a>-sidene.',
+	
+	'I want to translate Unchecky.':
+		'Jeg ønsker å oversette Unchecky.',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Oversettingsfilene til Unchecky-programmet og -nettsiden kan du finne på GitHub: <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a>',
+
+'How do I uninstall Unchecky?':
+		'Hvordan avinstallerer jeg Unchecky?',
+	'Unchecky kan bli avinstallert gjennom Windows Kontrollpanel, som et ethvert annet program. Se instruksene for å gjøre dette under '
+ for <a href="https://support.microsoft.com/nb-no/help/12425/windows-10-free-up-drive-space">Windows 10</a> ('Avinstallere apper du ikke bruker lenger'), <a href="https://support.microsoft.com/nb-no/help/17421/windows-free-up-drive-space">Windows 8, Windows 7</a> ('Avinstallere skrivebordsapper du ikke trenger lenger'), <a href="http://windows.microsoft.com/nb-no/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, eller <a href="https://support.microsoft.com/nb-no/kb/307895">Windows XP</a>.':
+		'',
+	'Hvis den vanlige metoden mislykkes, bør du da prøve <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, som er skreddersydd for å avinstallere korrumperte Unchecky-installasjoner.':
+		'',
+	
+	'Is it possible to install Unchecky silently?':
+		'',
+	'Ja, du kan kjøre veiviseren gjennom <code>-install</code>-tjenesten i kommandolinjen. For en liste over støttede kommandolinjetjenester, kjør <code>unchecky_setup.exe -?</code>.':
+		'',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Vennligst ikke bruk dette kontaktskjemaet til å melde om feil eller foreslå nye funksjoner. Send mye heller inn dine tilbakemeldinger på <a href="http://unchecky.userecho.com/">UserEcho</a>-sidene.',
+	
+	'First Name':
+		'Fornavn',
+	'Last Name':
+		'Etternavn',
+	'Email Address':
+		'E-post',
+	'Subject':
+		'Emne',
+	'Message':
+		'Melding',
+	'Send':
+		'Send',
+		
+	'Please fill in all fields.':
+		'Vennligst fyll ut alle feltene.',
+	'Please enter a valid email address.':
+		'Vennligst skriv inn en gyldig E-postadresse.',
+	'Could not complete your request.':
+		'Din forespørsel kunne ikke fullføres.',
+	'The message has been sent successfully.':
+		'Din melding har blitt sendt.',
+	
+	// 404
+	
+	'Not found':
+		'Siden finnes ikke',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Beklager, siden du ser etter kan ikke bli funnet.',
+	
+}
 	'Tired of unchecking unrelated offers while installing software?':
 		'Lei av å fjerne avhukinger for urelaterte tilbud når du installerer programvare?',
 	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
