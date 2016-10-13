8e603be275d45b79112e6022a65d63705f7ecf69
DandelionSprout <imreeil42@gmail.com>
2016-10-13 21:15:47 +0200

    Fixing nb_no.js
    I've tried to fix it here, by removing and/or moving around unnecessary lines, changed some text that seemed to be errenuously interpreted as Javascript commands, and made sure that all alternating lines (that I could find) ended in colons and commas.

--- website/nb_no.js
+++ website/nb_no.js
@@ -115,23 +115,22 @@ jquery_lang_js.prototype.lang.nb_no = {
 	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
 		'Send dine tilbakemeldinger på <a href="http://unchecky.userecho.com/">UserEcho</a>-sidene.',
 	
+	 'How do I uninstall Unchecky?':
+		'Hvordan avinstallerer jeg Unchecky?',
+	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
+	        'Unchecky kan bli avinstallert gjennom Windows Kontrollpanel, som et ethvert annet program. Se instruksene for å gjøre dette under for <a href="https://support.microsoft.com/nb-no/help/12425/windows-10-free-up-drive-space">Windows 10</a> ("Avinstallere apper du ikke bruker lenger"), <a href="https://support.microsoft.com/nb-no/help/17421/windows-free-up-drive-space">Windows 8, Windows 7</a> ("Avinstallere skrivebordsapper du ikke trenger lenger"), <a href="http://windows.microsoft.com/nb-no/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, eller <a href="https://support.microsoft.com/nb-no/kb/307895">Windows XP</a>.',
+        'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':,
+	        'Hvis den vanlige metoden mislykkes, bør du da prøve <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, som er skreddersydd for å avinstallere korrumperte Unchecky-installasjoner.',
+	
 	'I want to translate Unchecky.':
 		'Jeg ønsker å oversette Unchecky.',
 	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
 		'Oversettingsfilene til Unchecky-programmet og -nettsiden kan du finne på GitHub: <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a>',
-
-'How do I uninstall Unchecky?':
-		'Hvordan avinstallerer jeg Unchecky?',
-	'Unchecky kan bli avinstallert gjennom Windows Kontrollpanel, som et ethvert annet program. Se instruksene for å gjøre dette under '
- for <a href="https://support.microsoft.com/nb-no/help/12425/windows-10-free-up-drive-space">Windows 10</a> ('Avinstallere apper du ikke bruker lenger'), <a href="https://support.microsoft.com/nb-no/help/17421/windows-free-up-drive-space">Windows 8, Windows 7</a> ('Avinstallere skrivebordsapper du ikke trenger lenger'), <a href="http://windows.microsoft.com/nb-no/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, eller <a href="https://support.microsoft.com/nb-no/kb/307895">Windows XP</a>.':
-		'',
-	'Hvis den vanlige metoden mislykkes, bør du da prøve <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, som er skreddersydd for å avinstallere korrumperte Unchecky-installasjoner.':
-		'',
 	
 	'Is it possible to install Unchecky silently?':
-		'',
-	'Ja, du kan kjøre veiviseren gjennom <code>-install</code>-tjenesten i kommandolinjen. For en liste over støttede kommandolinjetjenester, kjør <code>unchecky_setup.exe -?</code>.':
-		'',
+		'Er det mulig å installere Unchecky på et usynlig vis?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+	        'Ja, du kan kjøre veiviseren gjennom <code>-install</code>-tjenesten i kommandolinjen. For en liste over støttede kommandolinjetjenester, kjør <code>unchecky_setup.exe -?</code>.',
 	
 	// Contact
 	
@@ -155,139 +154,6 @@ jquery_lang_js.prototype.lang.nb_no = {
 		'Vennligst fyll ut alle feltene.',
 	'Please enter a valid email address.':
 		'Vennligst skriv inn en gyldig E-postadresse.',
-	'Could not complete your request.':
-		'Din forespørsel kunne ikke fullføres.',
-	'The message has been sent successfully.':
-		'Din melding har blitt sendt.',
-	
-	// 404
-	
-	'Not found':
-		'Siden finnes ikke',
-	
-	'Sorry but the page you are looking for cannot be found.':
-		'Beklager, siden du ser etter kan ikke bli funnet.',
-	
-}
-	'Tired of unchecking unrelated offers while installing software?':
-		'Lei av å fjerne avhukinger for urelaterte tilbud når du installerer programvare?',
-	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
-		'Har du noen gang følt at, når du installerer programmvare, blir du forsøkt lurt til å installere tilleggsprogrammer du ikke ønsker? Har du noen gang ved et uhell ikke fjernet en avhuking av en sjekkboks, og brukt flere timer etterpå for å fjerne reklame-programvare? Har du noensinne åpnet nettleseren etter en installasjon og oppdaget at du har en ny startside, en ny søkemotor eller til og med en ny nettleser?<br>Unchecky forsøker å holde potensielt uønskede programmer unna din datamaskin.',
-	'Download':
-		'Last ned',
-	
-	'Introducing Unchecky!':
-		'Vi presenterer Unchecky!',
-	'Keeps your checkboxes clear.':
-		'Tømmer dine sjekkbokser.',
-	
-	'Unchecks':
-		'Krysser vekk',
-	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
-		'Unchecky fjerner automatisk avkryssing for urelaterte tilbud. Den sparer deg for flere museklikk og gjør det mindre sannsynlig å miste en sjekkboks for potensielt uønsket tilbud.',
-	
-	'Warns':
-		'Advarer',
-	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
-		'Installasjoner prøver ofte å lure inn ekstra programmvare som en naturlig del av installasjonen. Unchecky advarer deg når du prøver å akseptere et potensielt uønsket tilbud, noe som gjør at det er mindre sannsynlig for å akseptere ved et uhell.',
-	
-	'Updates automatically':
-		'Oppdateres automatisk',
-	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
-		'Installer og glem. Unchecky oppdateres automatisk til en hver tid når en ny versjon er tilgjengelig, slik at du ikke trenger å bekymre deg om du kjører siste versjon.',
-	
-	'Watch Unchecky in action':
-		'Se Unchecky i praksis',
-	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
-		'<a href="press#quotes">Se hva folk sier om Unchecky</a> eller <a href="press#videos">se flere videoer</a>.',
-	
-	// Press
-	
-	'People are talking about Unchecky':
-		'Det snakkes om Unchecky',
-	'Unchecky was also featured on the following international websites:':
-		'Unchecky var også kjennetegnet på følgende internasjonale websider:',
-	'on':
-		'på',
-	'Awards':
-		'Utmerkelser',
-	
-	'People are showing Unchecky in action':
-		'Folk viser Unchecky i aksjon',
-	'Demonstrating Unchecky':
-		'Demonstrasjon av Unchecky',
-	
-	'Portuguese':
-		'Portugisisk',
-	'Polish':
-		'Polsk',
-	'Russian':
-		'Russisk',
-	'Spanish':
-		'Spansk',
-	'Italian':
-		'Italiensk',
-	'French':
-		'Fransk',
-	'German':
-		'Tysk',
-	
-	// FAQ
-	
-	'What are the system requirements for Unchecky?':
-		'Hva med systemkravene for Unchecky?',
-	'Unchecky supports Windows XP and above.':
-		'Unchecky støtter Windows XP og senere.',
-	
-	'My antivirus flags Unchecky as malicious.':
-		'Mitt antivirus program sier at Unchecky er skadelig.',
-	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">was found 100% clean</a>.':
-		'Det er en falsk positiv. Vennligst rapporter dette til ditt antivirus selskap. Merk at Unchecky var nøye gjennomtestet av Softpedia og <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">funnet som 100% trygt</a>.',
-	
-	'What is the best way to report a bug or suggest a feature?':
-		'Hva er den beste måten å rapportere om en bug eller foreslå nye funksjoner?',
-	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
-		'Send dine tilbakemeldinger på <a href="http://unchecky.userecho.com/">UserEcho</a> sidene.',
-	
-	'How do I uninstall Unchecky?':
-		'',
-	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
-		'',
-	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
-		'',
-	
-	'I want to translate Unchecky.':
-		'Jeg ønsker å oversette Unchecky.',
-	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
-		'Oversettnings-filene til Unchecky programmet og websiden kan du finne på GitHub: <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a>',
-	
-	'Is it possible to install Unchecky silently?':
-		'',
-	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
-		'',
-	
-	// Contact
-	
-	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
-		'Vennligst ikke bruk dette kontaktskjemaet til å melde om feil eller foreslå nye funksjoner. Send dine tilbakemeldinger på <a href="http://unchecky.userecho.com/">UserEcho</a> sidene.',
-	
-	'First Name':
-		'Fornavn',
-	'Last Name':
-		'Etternavn',
-	'Email Address':
-		'E-post',
-	'Subject':
-		'Emne',
-	'Message':
-		'Melding',
-	'Send':
-		'Send',
-		
-	'Please fill in all fields.':
-		'Vennligst fyll ut alle feltene.',
-	'Please enter a valid email address.':
-		'Vennligst skriv inn en gyldig e-postadresse.',
 	'Could not complete your request.':
 		'Din forespørsel kunne ikke fullføres.',
 	'The message has been sent successfully.':
