7b21ce8da8695ac4375a48d941a7abbb7a64fe5b
RaMMicHaeL <mmm.maltsev@gmail.com>
2016-10-13 11:16:46 +0300

    Fixes for id.js

--- website/id.js
+++ website/id.js
@@ -1,194 +1,170 @@
-jquery_lang_js.prototype.lang.ca = {
-  
-  // Header
-  
-  'Home':
-    'Halaman Utama',
-  'Press':
-    'Pers',
-  'FAQ':
-    'Sering Ditanyakan',
-  'Changelog':
-    'Catatan Perubahan',
-  'Contact':
-    'Kontak',
-  'Links':
-    'Tautan',
-  'Bug/feature tracker':
-    'Laporan kesalahan/fitur',
-  'Blog':
-    'Blog',
-  'Language':
-    'Bahasa',
-  
-  // Footer
-  
-  'Copyright © 2016 Reason Company Software Inc.':
-    'Hak Cipta © 2016 Reason Company Software Inc.',
-  
-  // Legal
-  
-  'End-User License Agreement':
-    'Perjanjian Lisensi Terhadap Pengguna Akhir',
-  'Privacy Policy':
-    'Kebijakan Privasi',
-  
-  // Home
-  
-  'Tired of unchecking unrelated offers while installing software?':
-    'Lelah membatalkan penawaran yang tidak sesuai ketika memasang perangkat lunak?',
-  'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
-    'Pernahkah Anda merasa ketika memasang perangkat lunak, program pemasang mencoba memaksa Anda memasang program tambahan yang tidak diinginkan? Pernah kelewatan saat menghapus centang dan memakan waktu lama setelahnya untuk menghapus perangkat lunak iklan? Pernah membuka peramban Anda setelah pemasangan hanya untuk memastikan bahwa Anda memiliki halaman muka, mesin pencari atau bahkan peramban baru?<br>Unchecky bertujuan untuk menjaga perangkat lunak yang tidak diinginkan tidak terpasang di komputer Anda.',
-  'Download':
-    'Unduh',
-  
-  'Introducing Unchecky!':
-    'Perkenalkan Unchecky!',
-  'Keeps your checkboxes clear.':
-    'Membuat kotak centang Anda bersih.',
-  
-  'Unchecks':
-    'Hapus centang',
-  'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
-    'Unchecky secara otomatis menghapus penawaran yang tidak sesuai, menghemat waktu untuk menekan tetikus Anda dan meminimalkan kemungkinan luput dalam menghapus centang pada penawaran yang tidak sesuai.',
-  
-  'Warns':
-    'Peringatan',
-  'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
-    'Program pemasang mencoba menyelinap sebagai program tambahan sebagai bagian dari proses pemasangan perangkat lunak. Unchecky memperingkatkan Anda ketika Anda mencoba untuk menyetujui sebuah penawaran yang tidak diinginkan, di mana akan mengurangi kemungkinan untuk setuju secara tidak sengaja.',
-  
-  'Updates automatically':
-    'Pembaruan otomatis',
-  'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
-    'Pasang dan lupakan. Unchecky otomatis memperbarui dirinya ketika sebuah versi tersedia, sehingga Anda tidak perlu risau untuk selalu menjalankan versi terbaru.',
-  
-  'Watch Unchecky in action':
-    'Lihat Unchecky dalam aksi',
-  '<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
-    '<a href="press#quotes">Lihat apa kata orang tentang Unchecky</a> atau <a href="press#videos">lihat video lainnya</a>.',
-  
-  // Press
-  
-  'People are talking about Unchecky':
-    'Pendapat orang tentang Unchecky',
-  'Unchecky was also featured on the following international websites:':
-    'Unchecky juga ditampilkan dalam situs web internasional berikut ini:',
-  'on':
-    'pada',
-  'Awards':
-    'Penghargaan',
-  
-  'People are showing Unchecky in action':
-    'Video Unchecky dari pengguna',
-  'Demonstrating Unchecky':
-    'Menunjukkan Unchecky',
-  
-  'German':
-    'Jerman',
-  'English':
-    'Inggris',
-  'Bulgarian':
-    'Bulgaria',
-  'Catalan':
-    'Catala',
-  'Danish':
-    'Danish',
-  'Slovenian':
-    'Slovenia',
-  'Spanish':
-    'Spanyol',
-  'French':
-    'Perancis',
-  'Indonesia':
-    'Indonesia',
-  'Italian':
-    'Italia',
-  'Japanese':
-    'Jepang',
-  'Dutch':
-    'Belanda',
-  'Norwegian':
-    'Norwegia',
-  'Polish':
-    'Polandia',
-  'Portuguese':
-    'Portugis',
-  'Russian':
-    'Rusia',
-  'Swedish':
-    'Swedia',
-  'Turkish':
-    'Turki',
-  'Chinese':
-    'Cina',
-  
-  // FAQ
-  
-  'What are the system requirements for Unchecky?':
-    'Apa kebutuhan sistem yang diperlukan Unchecky?',
-  'Unchecky supports Windows XP and above.':
-    'Unchecky mendukung Windos XP dan versi di atasnya.',
-  
-  'My antivirus flags Unchecky as malicious.':
-    'Antivirus saya mendeteksi Unchecky sebagai program jahat.',
-  'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">was found 100% clean</a>.':
-    'Itu merupakan deteksi <i>false positive</i>. Silahkan laporkan terkait hal ini kepada perusahaan antivirus Anda. Perlu dicatat bahwa Unchecky telah diuji oleh Softpedia dan <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">dinyatakan bahwa 100% bebas virus</a>.',
-  
-  'What is the best way to report a bug or suggest a feature?':
-    'Apa cara terbaik untuk melaporkan kesalahan program atau menyarankan sebuah fitur baru?',
-  'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
-    'Beritahukan umpan balik Anda melalui <a href="http://unchecky.userecho.com/">situs UserEcho</a>.',
-  
-  'How do I uninstall Unchecky?':
-    'Bagaimana cara menghapus Unchecky?',
-  'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
-    'Unchecky dapat dihapus dari Windows Control Panel seperti perangkat lunak yang lain. Lihat instruksi penghapusan untuk <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, atau <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.',
-  'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
-    'Jika metode standar penghapusan di atas gagal, silahkan coba gunakan <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a> yang telah didesain untuk menghapus pemasang Unchecky yang rusak.',
-  
-  'I want to translate Unchecky.':
-    'Saya ingin menerjemahkan Unchecky.',
-  'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
-    'Berkas terjemahan pada perangkat lunak dan situs web Unchecky dapat ditemukan di <a href="https://github.com/RaMMicHaeL/unchecky-translate">GitHub unchecky/unchecky-translate</a>.',
-  
-  'Is it possible to install Unchecky silently?':
-    'Apakah mungkin untuk memasang Unchecky tanpa antar muka pengguna?',
-  'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
-    'Ya, jalankan perangkat lunak pemasang (<i>installer</i>) dengan parameter <code>install</code> pada <i>command line</i>. Untuk mengetaui daftar lengkap parameter untuk <i>command line</i> jalankan <code>unchecky_setup.exe -?</code>.',
-  
-  // Contact
-  
-  'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
-    'Dimohon untuk tidak menggunakan kotak isian kontak ini untuk melaporkan kesalahan program atau menyarankan fitur baru. Kirim umpan balik ada pada <a href="http://unchecky.userecho.com/">halaman UserEcho</a>.',
-  
-  'First Name':
-    'Nama Depan',
-  'Last Name':
-    'Nama Belakang',
-  'Email Address':
-    'Alamat Surat Elektronik',
-  'Subject':
-    'Subjek',
-  'Message':
-    'Isi Pesan',
-  'Send':
-    'Kirim',
-  
-  'Please fill in all fields.':
-    'Mohon isi semua kotak isian.',
-  'Please enter a valid email address.':
-    'Mohon isi alamat email yang valid.',
-  'Could not complete your request.':
-    'Tidak dapat memproses permintaan Anda.',
-  'The message has been sent successfully.':
-    'Pesan telah sukses dikirim.',
-  
-  // 404
-  
-  'Not found':
-    'Tidak ditemukan',
-  
-  'Sorry but the page you are looking for cannot be found.':
-    'Maaf, halaman yang Anda cari tidak dapat ditemukan.',
-  
+jquery_lang_js.prototype.lang.id = {
+	
+	// Header
+	
+	'Home':
+		'Halaman Utama',
+	'Press':
+		'Pers',
+	'FAQ':
+		'Sering Ditanyakan',
+	'Changelog':
+		'Catatan Perubahan',
+	'Contact':
+		'Kontak',
+	'Links':
+		'Tautan',
+	'Bug/feature tracker':
+		'Laporan kesalahan/fitur',
+	'Blog':
+		'Blog',
+	'Language':
+		'Bahasa',
+	
+	// Footer
+	
+	'Copyright © 2016 Reason Company Software Inc.':
+		'Hak Cipta © 2016 Reason Company Software Inc.',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Perjanjian Lisensi Terhadap Pengguna Akhir',
+	'Privacy Policy':
+		'Kebijakan Privasi',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Lelah membatalkan penawaran yang tidak sesuai ketika memasang perangkat lunak?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Pernahkah Anda merasa ketika memasang perangkat lunak, program pemasang mencoba memaksa Anda memasang program tambahan yang tidak diinginkan? Pernah kelewatan saat menghapus centang dan memakan waktu lama setelahnya untuk menghapus perangkat lunak iklan? Pernah membuka peramban Anda setelah pemasangan hanya untuk memastikan bahwa Anda memiliki halaman muka, mesin pencari atau bahkan peramban baru?<br>Unchecky bertujuan untuk menjaga perangkat lunak yang tidak diinginkan tidak terpasang di komputer Anda.',
+	'Download':
+		'Unduh',
+	
+	'Introducing Unchecky!':
+		'Perkenalkan Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Membuat kotak centang Anda bersih.',
+	
+	'Unchecks':
+		'Hapus centang',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky secara otomatis menghapus penawaran yang tidak sesuai, menghemat waktu untuk menekan tetikus Anda dan meminimalkan kemungkinan luput dalam menghapus centang pada penawaran yang tidak sesuai.',
+	
+	'Warns':
+		'Peringatan',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Program pemasang mencoba menyelinap sebagai program tambahan sebagai bagian dari proses pemasangan perangkat lunak. Unchecky memperingkatkan Anda ketika Anda mencoba untuk menyetujui sebuah penawaran yang tidak diinginkan, di mana akan mengurangi kemungkinan untuk setuju secara tidak sengaja.',
+	
+	'Updates automatically':
+		'Pembaruan otomatis',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Pasang dan lupakan. Unchecky otomatis memperbarui dirinya ketika sebuah versi tersedia, sehingga Anda tidak perlu risau untuk selalu menjalankan versi terbaru.',
+	
+	'Watch Unchecky in action':
+		'Lihat Unchecky dalam aksi',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Lihat apa kata orang tentang Unchecky</a> atau <a href="press#videos">lihat video lainnya</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Pendapat orang tentang Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky juga ditampilkan dalam situs web internasional berikut ini:',
+	'on':
+		'pada',
+	'Awards':
+		'Penghargaan',
+	
+	'People are showing Unchecky in action':
+		'Video Unchecky dari pengguna',
+	'Demonstrating Unchecky':
+		'Menunjukkan Unchecky',
+	
+	'Portuguese':
+		'Portugis',
+	'Polish':
+		'Polandia',
+	'Russian':
+		'Rusia',
+	'Spanish':
+		'Spanyol',
+	'Italian':
+		'Italia',
+	'French':
+		'Perancis',
+	'German':
+		'Jerman',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Apa kebutuhan sistem yang diperlukan Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky mendukung Windos XP dan versi di atasnya.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Antivirus saya mendeteksi Unchecky sebagai program jahat.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">was found 100% clean</a>.':
+		'Itu merupakan deteksi <i>false positive</i>. Silahkan laporkan terkait hal ini kepada perusahaan antivirus Anda. Perlu dicatat bahwa Unchecky telah diuji oleh Softpedia dan <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml">dinyatakan bahwa 100% bebas virus</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Apa cara terbaik untuk melaporkan kesalahan program atau menyarankan sebuah fitur baru?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Beritahukan umpan balik Anda melalui <a href="http://unchecky.userecho.com/">situs UserEcho</a>.',
+	
+	'How do I uninstall Unchecky?':
+		'Bagaimana cara menghapus Unchecky?',
+	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, or <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.':
+		'Unchecky dapat dihapus dari Windows Control Panel seperti perangkat lunak yang lain. Lihat instruksi penghapusan untuk <a href="http://windows.microsoft.com/en-us/windows-8/uninstall-change-program">Windows 8</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-7">Windows 7</a>, <a href="http://windows.microsoft.com/en-us/windows/uninstall-change-program#uninstall-change-program=windows-vista">Windows Vista</a>, atau <a href="https://support.microsoft.com/en-us/kb/307895">Windows XP</a>.',
+	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
+		'Jika metode standar penghapusan di atas gagal, silahkan coba gunakan <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a> yang telah didesain untuk menghapus pemasang Unchecky yang rusak.',
+	
+	'I want to translate Unchecky.':
+		'Saya ingin menerjemahkan Unchecky.',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/RaMMicHaeL/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Berkas terjemahan pada perangkat lunak dan situs web Unchecky dapat ditemukan di <a href="https://github.com/RaMMicHaeL/unchecky-translate">GitHub unchecky/unchecky-translate</a>.',
+	
+	'Is it possible to install Unchecky silently?':
+		'Apakah mungkin untuk memasang Unchecky tanpa antar muka pengguna?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Ya, jalankan perangkat lunak pemasang (<i>installer</i>) dengan parameter <code>install</code> pada <i>command line</i>. Untuk mengetaui daftar lengkap parameter untuk <i>command line</i> jalankan <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Dimohon untuk tidak menggunakan kotak isian kontak ini untuk melaporkan kesalahan program atau menyarankan fitur baru. Kirim umpan balik ada pada <a href="http://unchecky.userecho.com/">halaman UserEcho</a>.',
+	
+	'First Name':
+		'Nama Depan',
+	'Last Name':
+		'Nama Belakang',
+	'Email Address':
+		'Alamat Surat Elektronik',
+	'Subject':
+		'Subjek',
+	'Message':
+		'Isi Pesan',
+	'Send':
+		'Kirim',
+	
+	'Please fill in all fields.':
+		'Mohon isi semua kotak isian.',
+	'Please enter a valid email address.':
+		'Mohon isi alamat email yang valid.',
+	'Could not complete your request.':
+		'Tidak dapat memproses permintaan Anda.',
+	'The message has been sent successfully.':
+		'Pesan telah sukses dikirim.',
+	
+	// 404
+	
+	'Not found':
+		'Tidak ditemukan',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Maaf, halaman yang Anda cari tidak dapat ditemukan.',
+	
 }
