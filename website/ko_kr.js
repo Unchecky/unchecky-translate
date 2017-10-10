8c0d7abe5550a51e89196ca8e4753d342272db6a
RaMMicHaeL <mmm.maltsev@gmail.com>
2017-10-10 20:56:52 +0300

    Updated the Korean translation of the program and the website

--- website/ko_kr.js
+++ website/ko_kr.js
@@ -7,15 +7,15 @@ jquery_lang_js.prototype.lang.ko_kr = {
 	'Press':
 		'기사',
 	'FAQ':
-		'자주하는질문',
+		'잦은 질답',
 	'Changelog':
-		'변경사항',
+		'변경 사항',
 	'Contact':
-		'지원',
+		'고객 지원',
 	'Links':
 		'링크',
 	'Bug/feature tracker':
-		'버그/기능트래커',
+		'버그 보고/기능 제안',
 	'Blog':
 		'블로그',
 	'Language':
@@ -24,55 +24,55 @@ jquery_lang_js.prototype.lang.ko_kr = {
 	// Footer
 	
 	'Copyright © 2017 Reason Company Software Inc.':
-		'Copyright © 2017 Reason Company Software Inc. 한국어： <a href="mailto:ukjinplant@msn.com">컴포지트</a>',
+		'저작권 © 2017 Reason Company Software Inc. 한국어： <a href="mailto:kolanp@gmail.com">Jaehyung Lee</a>',
 	
 	// Legal
 	
 	'End-User License Agreement':
-		'최종 사용자 계약서',
+		'최종 사용자 사용권 계약',
 	'Privacy Policy':
-		'개인정보보호정책',
+		'개인정보 보호정책',
 	
 	// Home
 	
 	'Tired of unchecking unrelated offers while installing software?':
-		'여러분은 소프트웨어를 설치하는 동안 원치 않는 추가 소프트웨어 체크해제 하십니까?',
+		'소프트웨어를 설치할 때마다 추가로 제공되는 번들 소프트웨어 설치 옵션에 체크 해제하기 불편하시죠?',
 	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
-		'여러분은 아직도 소프트웨어를 설치하는 동안 원치 않는 추가 프로그램을 처리하느라 피곤하십니까? 체크 박스를 깜박해서 애드웨어 지우느라 고생하시죠? 게다가 설치 후 브라우저를 실행하면 홈페이지가 바뀌거나, 검색 엔진이 바뀌거나, 못보던 브라우저 때문에 골치아프십니까?<br>Unchecky가 여러분 컴퓨터에 원치 않는 프로그램으로부터 뒤에서 지속적으로 지키는 목표를 가지고 태어났습니다.',
+		'소프트웨어를 설치할 때 불필요한 번들 프로그램이 추가로 설치되려고 한 경험이 있으신가요? 이러한 번들 프로그램 설치 옵션 체크 상자에 체크 해제하는 것을 깜빡하여 나중에 애드웨어를 제거하느라 시간을 허비하신 적이 있으신가요? 설치 후 웹 브라우저를 열었을 때 홈페이지나 검색 엔진이 변경되었거나 전혀 다른 웹 브라우저가 열리는 경험을 하신 적이 있으신가요? <br>Unchecky는 컴퓨터에 잠재적으로 불필요한 프로그램의 설치를 방지하는 것이 목표입니다.',
 	'Download':
-		'내려받기',
+		'다운로드',
 	
 	'Introducing Unchecky!':
-		'Unchecky를 소개합니다！',
+		'Unchecky를 소개합니다',
 	'Keeps your checkboxes clear.':
-		'체크박스를 모두 해제시켜 드립니다.',
+		'체크 상자를 체크 해제해 드립니다.',
 	
 	'Unchecks':
-		'체크해제',
+		'체크 해제',
 	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
-		'Unchecky는 자동으로 원치 않는 제공에 대한 체크박스를 해제시켜 불필요한 마우스 클릭과 싫증나는 제공 소프트웨어 체크박스로부터 여러분을 해방시켜 드립니다.',
+		'Unchecky는 추가로 설치되는 프로그램의 설치 여부를 확인하는 체크 상자에 자동으로 체크를 해제해줍니다. 사용자의 마우스 클릭 수를 줄이고 원치 않는 프로그램이 설치되는 것을 예방할 수 있습니다.',
 	
 	'Warns':
-		'주의',
+		'경고',
 	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
-		'설치 관리자가 가끔 추가 프로그램을 정상적 과정으로 속일 수 있습니다. Unchecky는 이런 잠재적 원치 않는 소프트웨어를 설치할 경우 실수로 승인하지 않도록 주의시켜 드립니다.',
+		'일부 설치 프로그램들은 종종 설치 과정에서 추가적인 번들 프로그램 설치를 정상적인 설치 부분인양 오인할 수 있게 제작됩니다. Unchecky는 사용자가 이러한 잠재적으로 불필요한 프로그램의 설치에 동의하려 할 때 알려드림으로써 무심코 설치할 가능성이 줄어듭니다.',
 	
 	'Updates automatically':
 		'자동 업데이트',
 	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
-		'설치만 하고 잊으셔도 됩니다. Unchecky가 자동으로 새 버전으로 판올림하여 지속적으로 최신 버전을 유지시킬 수 있습니다.',
+		'설치하고 잊으버리셔도 됩니다. Unchecky가 자동으로 업데이트되어 항상 새 버전을 사용할 수 있습니다. 따라서 최신 버전으로 유지하는 것에 대해 걱정하지 않으셔도 됩니다.',
 	
 	'Watch Unchecky in action':
-		'Unchecky 동영상',
+		'Unchecky 사용 동영상',
 	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
-		'<a href="press#quotes">사람들이 말하는 Unchecky</a> 또는 <a href="press#videos">비디오 더 보기</a>.',
+		'<a href="press#quotes">Unchecky에 대한 평판 보기</a> 또는 <a href="press#videos">동영상 더 보기</a>.',
 	
 	// Press
 	
 	'People are talking about Unchecky':
-		'사람들이 말하는 Unchecky',
+		'Unchecky에 대한 평판',
 	'Unchecky was also featured on the following international websites:':
-		'Unchecky가 다음 국제 웹사이트에 소개되고 있습니다:',
+		'Unchecky는 다음의 국제 웹사이트에 소개되었습니다:',
 	'on':
 		'님,',
 	'Awards':
@@ -81,7 +81,7 @@ jquery_lang_js.prototype.lang.ko_kr = {
 	'People are showing Unchecky in action':
 		'사람들의 Unchecky 사용 동영상',
 	'Demonstrating Unchecky':
-		'Unchecky 시연',
+		'Unchecky 사용법 시연',
 	
 	'Portuguese':
 		'포르투갈어',
@@ -101,41 +101,41 @@ jquery_lang_js.prototype.lang.ko_kr = {
 	// FAQ
 	
 	'What are the system requirements for Unchecky?':
-		'Unchecky의 시스템 요구 사항은?',
+		'Unchecky의 시스템 요구 사항은 무엇인가요?',
 	'Unchecky supports Windows XP and above.':
-		'Unchecky는 Windows XP 이상에서 동작합니다.',
+		'Unchecky는 Windows XP와 상위 버전을 지원합니다.',
 	
 	'My antivirus flags Unchecky as malicious.':
-		'내 백신 프로그램이 Unchecky를 악성코드로 취급합니다.',
+		'내 안티바이러스가 Unchecky를 악성으로 진단합니다.',
 	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#status">was found 100% clean</a>.':
-		'백신의 오진입니다. 백신 업체에 문의해 주십시오. Unchecky는 Softpedia 에서 <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#status">100% 깨끗함을 입증하였습니다</a>.',
+		'안티바이러스의 오진입니다. 안티바이러스 업체에 오진으로 보고해주십시오. Unchecky는 Softpedia에서 철저하게 테스트되었으며 <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#status">100% 무결함</a>으로 인증되었음을 알려드립니다.',
 	
 	'What is the best way to report a bug or suggest a feature?':
-		'버그를 보고하거나 새로운 기능을 제안하고 싶다면?',
+		'버그 보고나 기능 제안 제안을 위한 가장 좋은 방법은 무엇인가요?',
 	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
-		'<a href="http://unchecky.userecho.com/">UserEcho</a> 페이지에서 피드백이 가능합니다.',
+		'<a href="http://unchecky.userecho.com/">UserEcho 페이지</a>에서 귀하의 피드백을 게시하여 주시면 됩니다.',
 	
 	'How do I uninstall Unchecky?':
-		'Unchecky를 제거하는 방법은?',
+		'Unchecky를 어떻게 제거하나요?',
 	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs">Windows 10</a> or <a href="https://support.microsoft.com/en-us/kb/2601726">Windows 7</a>.':
-		'Unchecky는 제어판의 프로그램 추가/삭제 또는 프로그램 제거 에서 제거 가능합니다. 각 운영체제별 프로그램 제거하는 방법은 <a href="https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs">Windows 10</a> 또는 <a href="https://support.microsoft.com/en-us/kb/2601726">Windows 7</a> 를 참조하십시오.',
+		'Unchecky는 Windows 제어판의 프로그램 및 기능에서 제거할 수 있습니다. 각 운영 체제별 프로그램 제거 방법은 다음을 참조하십시오: <a href="https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs">Windows 10</a> 또는 <a href="https://support.microsoft.com/en-us/kb/2601726">Windows 7</a>',
 	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
-		'만약 정상적인 제거가 실패했을 경우、 <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a> 을 사용해 주십시오.',
+		'표준 제거 방식이 실패되었을 경우, 손상된 Unchecky 설치를 제거하기 위해 제작된 <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>을 사용하십시오.',
 	
 	'I want to translate Unchecky.':
 		'Unchecky를 번역하고 싶습니다.',
 	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a> GitHub repository.':
-		'Unchecky 프로그램과 웹 사이를 번역할 수 있는 텍스트 파일을 <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a> 에서 제공합니다.',
+		'Unchecky 프로그램과 웹사이의 번역 파일은 GitHub 저장소 <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a>에서 찾을 수 있습니다.',
 	
 	'Is it possible to install Unchecky silently?':
-		'Unchecky를 조용하게 바로 설치 가능한가요?',
+		'Unchecky를 자동 설치할 수 있나요?',
 	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
-		'네. 명령줄에 <code>-install</code> 인자를 추가하시면 됩니다. 지원되는 프로그램 인자를 확인하고자 한다면 <code>unchecky_setup.exe -?</code>를 입력하십시오.',
+		'예, <code>-install</code> 커맨드 라인 스위치로 설치기를 실행합니다. 지원하는 커맨드 라인 스위치의 목록은 <code>unchecky_setup.exe -?</code>를 실행합니다.',
 	
 	// Contact
 	
 	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
-		'여기에 버그나 제안 사항을 전달할 수 없습니다. 피드백을 전하고자 한다면 <a href="http://unchecky.userecho.com/">UserEcho 페이지</a>를 이용하십시오.',
+		'버그 보고나 기능 제안을 위해 이 양식을 사용하지 마십시오. 버그 보고나 제안 사항을 게시하려면, <a href="http://unchecky.userecho.com/">UserEcho 페이지</a>를 이용하십시오.',
 	
 	'First Name':
 		'이름',
@@ -151,20 +151,20 @@ jquery_lang_js.prototype.lang.ko_kr = {
 		'보내기',
 	
 	'Please fill in all fields.':
-		'모든 양식을 채워 주십시오.',
+		'모든 항목을 채워 주십시오.',
 	'Please enter a valid email address.':
 		'올바른 이메일 주소를 입력하십시오.',
 	'Could not complete your request.':
-		'요청을 처리할 수 없습니다.',
+		'요청을 완료할 수 없습니다.',
 	'The message has been sent successfully.':
-		'메시지를 성공적으로 보냈습니다.',
+		'귀하의 메시지가 성공적으로 전송되었습니다.',
 	
 	// 404
 	
 	'Not found':
-		'찾을 수 없는 페이지',
+		'404 오류 - 찾을 수 없음',
 	
 	'Sorry but the page you are looking for cannot be found.':
-		'죄송합니다. 찾으려는 페이지가 여기에 없습니다.',
+		'죄송하지만 찾으시는 페이지가 삭제되었거나 이동되었을 수 있습니다. 사이트 관리자에게 문의하여 주십시오.',
 	
 }
