1ca42e4eac24c9476a98da0abc47e5ff268eacc7
RaMMicHaeL <mmm.maltsev@gmail.com>
2018-05-07 11:26:59 +0300

    Romanian translation of the website

---
+++ website/ro.js
@@ -0,0 +1,170 @@
+jquery_lang_js.prototype.lang.ro = {
+	
+	// Header
+	
+	'Home':
+		'Acasă',
+	'Press':
+		'Presă',
+	'FAQ':
+		'FAQ',
+	'Changelog':
+		'Istoric al modificărilor',
+	'Contact':
+		'Contact',
+	'Links':
+		'Legături externe',
+	'Bug/feature tracker':
+		'Defecțiuni/caracteristici',
+	'Blog':
+		'Blog',
+	'Language':
+		'Limba',
+	
+	// Footer
+	
+	'Copyright © 2018 Reason Company Software Inc.':
+		'Copyright © 2018 Reason Company Software Inc. Traducere română : <a href="mailto:pro.internet.transilvania@gmail.com">Eugen</a>',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Contract de licențiere pentru utilizatorul final',
+	'Privacy Policy':
+		'Politica de confidențialitate',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Ați obosit debifând ofertele colaterale când instalați software legitim ?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'Ați pățit uneori ca instalând un software legitim acesta să adauge programe nedorite? Vi s-a întâmplat ca ne-debifând nicio casetă de selectare ați petrecut ulterior ore întregi pentru dezinstalarea soft-ului nedorit? Ați deschis browserul după o instalare și ați observat că pagina de start s-a modificat, fie cu ul alt motor de căutare sau chiar cu alt browser?<br>Unchecky urmărește să țină la distanță astfel de potențiale programe nedorite (PUP).',
+	'Download':
+		'Descărcare',
+	
+	'Introducing Unchecky!':
+		'Prezentare Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Păstrează caselele de selectare debifate.',
+	
+	'Unchecks':
+		'Debifează',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky debifează automat ofertele colaterale, economisind clicuri și evitând instalarea neintenționată a programelor nedorite.',
+	
+	'Warns':
+		'Avertisemente',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Unele programe de instalare încearcă să adauge alte aplicații în procesul natural de instalare, ca făcând parte din proces. Unchecky avertizează atunci când sunteți pe cale să acceptați instalarea unui program potențial nedorit, ceea ce va face imposibilă instalarea acestuia fără intenție.',
+	
+	'Updates automatically':
+		'Actualizare automată',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Instalează și uită. Unchecky se va actualiza automat atunci când o nouă versiune devine disponibiliă, deci nu trebuie să vă faceți griji că nu ve-ți rula cea mai recentă versiune.',
+	
+	'Watch Unchecky in action':
+		'Urmăriți Unchecky în acțiune',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Ce zic cei ce folosesc Unchecky</a> sau <a href="press#videos">Urmariți mai multe secvențe video</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Oamenii vorbesc despre Unchecky',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky este de asemenea evidențiat pe site-urile Internaționale pe Internet :',
+	'on':
+		'pe',
+	'Awards':
+		'Récunoașteri',
+	
+	'People are showing Unchecky in action':
+		'Oamenii arată Unchecky în acțiune',
+	'Demonstrating Unchecky':
+		'Demonstrație Unchecky',
+	
+	'Portuguese':
+		'Portugheză',
+	'Polish':
+		'Poloneză',
+	'Russian':
+		'Rusă',
+	'Spanish':
+		'Spaniolă',
+	'Italian':
+		'Italiană',
+	'French':
+		'Franceză',
+	'German':
+		'Germană',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Care sunt cerințele de sistem pentru Unchecky ?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky suportă Windows XP sau variante superioare.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Antivirusul meu detectează Unchecky ca fiind rău intenționat.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#status">was found 100% clean</a>.':
+		'Este fals pozitiv. Vă rugăm notificați furnizorul dvs. de antivirus. Luați în considerare că Unchecky a fost testat de Softpedia și a fost declarat <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#status">100% fără riscuri</a>',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Care este cea mai bună metodă de a semnala o defecțiune sau o îmbunătățire ?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Postați sugestia dumneavoastră la <a href="http://unchecky.userecho.com/">pagina UserEcho</a>',
+	
+	'How do I uninstall Unchecky?':
+		'Cum să dezinstalez Unchecky ',
+	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs">Windows 10</a> or <a href="https://support.microsoft.com/en-us/kb/2601726">Windows 7</a>.':
+		'Unchecky poate fi dezinstalat din Panoul de Control Windows, ca oricare alt prograsm. Pentru mai multe detalii, apăsați <a href="https://support.microsoft.com/fr-fr/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs">pentru Windows 10</a> sau <a href="https://support.microsoft.com/fr-fr/kb/2601726">pentru Windows 7</a>.',
+	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
+		'Dacă metoda standard eșuează, puteți utiliza <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a> care vă permite să dezinstalați programul Unchecky defectat',
+	
+	'I want to translate Unchecky.':
+		'Vreau să traduc Unchecky :',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Fișierele de tradus pentru Unchecky și site-ul de unde puteți descărca este GitHub : <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a>',
+	
+	'Is it possible to install Unchecky silently?':
+		'Este posibil să instalez silențios Unchecky ?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Da, rulați programul de instalare cu argumentul <code>-install</code>. Pentru lista completă de argumente disponibile, lansați <code>unchecky_setup.exe -?</code>.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Vă rugăm nu utilizați formularul de contact pentru a semnala defecțiuni sau propuneri de funcții noi. Publicați sugestiile dvs. la <a href="http://unchecky.userecho.com/">pagina UserEcho</a>.',
+	
+	'First Name':
+		'Prenume',
+	'Last Name':
+		'Nume',
+	'Email Address':
+		'Adresa de eMail',
+	'Subject':
+		'Subiect',
+	'Message':
+		'Mesaj',
+	'Send':
+		'Trimite',
+	
+	'Please fill in all fields.':
+		'Vă rugăm completați toate câmpurile.',
+	'Please enter a valid email address.':
+		'Vă rugăm introduceți o adresă de eMail validă.',
+	'Could not complete your request.':
+		'Solicitarea dvs. nu s-a putut finaliza.',
+	'The message has been sent successfully.':
+		'Mesajul a fost expediat cu succes.',
+	
+	// 404
+	
+	'Not found':
+		'Pagină negăsită',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Ne pare rău, pagina solicitată nu a fost găsită.',
+	
+}
