6d1bfb49f930f8986954306188f55951b5c0f40b
nikola nikola <wwenikola123@gmail.com>
2021-04-29 17:34:35 +0200

    Added Serbian translation.

---
+++ website/sr.js
@@ -0,0 +1,175 @@
+jquery_lang_js.prototype.lang.sr = {
+	
+	// Header
+	
+	'Home':
+		'Početna',
+	'Press':
+		'Objave',
+	'FAQ':
+		'Česta pitanja',
+	'Changelog':
+		'Novosti u ažuriranjima',
+	'Contact':
+		'Kontakt',
+	'Links':
+		'Linkovi',
+	'Bug/feature tracker':
+		'Prijava grešaka i predlog novih funkcija',
+	'Blog':
+		'Blog',
+	'Language':
+		'Jezik',
+	
+	// Footer
+	
+	'Copyright © 2019 Reason Company Software Inc.':
+		'Copyright © 2019 Reason Company Software Inc.',
+	
+	// Legal
+	
+	'End-User License Agreement':
+		'Licencni ugovor sa krajnjim korisnikom',
+	'Privacy Policy':
+		'Politika privatnosti',
+	
+	// Home
+	
+	'Tired of unchecking unrelated offers while installing software?':
+		'Umorni ste dečekiranja ponuda u instalacionim programima?',
+	'Have you ever felt, while installing software, that the installer tries to push additional unwanted programs at all cost? Ever missed a checkbox, and spent hours afterwards removing adware? Ever opened your browser after an installation, only to find out that you have a new homepage, a new search engine, or even a new browser?<br>Unchecky aims to keep potentially unwanted programs out of your computer.':
+		'da li ste se ikada osećali, prilikom instalacije programa, da Vas instalacioni program tera da instalirate neki neželjeni program? Da li ste ikada slučajno prevideli ponudu i onda potrošili sate i sate na brisanje adwarea? Da li ste ikada nakon instalacije otvorili Internet pretraživač, i onda shvatili da je program promenio početnu stranicu, podrazumevanu pretragu ili čak sam Web pretraživač?<br>Unchecky štiti vaš računar od potencionalno neželjenih programa.',
+	'Download':
+		'Preuzimanje',
+	
+	'Introducing Unchecky!':
+		'Predstavljamo Vam Unchecky!',
+	'Keeps your checkboxes clear.':
+		'Drži vaša izborna polja dečekiranim',
+	
+	'Unchecks':
+		'Dečekira',
+	'Unchecky automatically unchecks unrelated offers, both saving you mouse clicks and making it less likely to miss a checkbox of an unwanted offer.':
+		'Unchecky automatski dečekira ponude iz instalacionih programa. Štedi Vaše klikove i štiti Vas da vam više nikada ne promakne niti jedna ponuda.',
+	
+	'Warns':
+		'Upozorava',
+	'Installers often try to sneak additional programs as a natural part of the installation. Unchecky warns you when you try to accept a potentially unwanted offer, which makes it less likely to be accepted accidentally.':
+		'Instalacioni programi često znaju progurati razne programe u običnoj instalaciji. Unchecky Vas upozorava kada pokušate prihvatiti instalaciju nepoželjnih programa, što umanjuje šansu da propustite ponudu.',
+	
+	'Updates automatically':
+		'Ažurira se automatski',
+	'Install and forget. Unchecky automatically updates whenever a new version is available, so you don\'t have to worry about running the latest version.':
+		'Instaliraj i zaboravi. Unchecky se ažurira automatski ako je nova verzija dostupna za preuzimanje, pa se ne morate brinuti da li imate najnoviju verziju.',
+	
+	'Watch Unchecky in action':
+		'Pogledajte Unchecky u akciji',
+	'<a href="press#quotes">See what people are saying about Unchecky</a> or <a href="press#videos">watch more videos</a>.':
+		'<a href="press#quotes">Pogledajte šta drugi kažu o Uncheckyju</a> ili <a href="press#videos">gledajte više videa</a>.',
+	
+	// Press
+	
+	'People are talking about Unchecky':
+		'Ljudi pričaju o Uncheckyju',
+	'Unchecky was also featured on the following international websites:':
+		'Unchecky je takođe spominjan na sledećim međunarodnim sajtovima:',
+	'on':
+		'na',
+	'Awards':
+		'Nagrade',
+	
+	'People are showing Unchecky in action':
+		'Ljudi pokazuju Unchecky u akciji',
+	'Demonstrating Unchecky':
+		'Demonstracija Uncheckyja',
+	
+	'Portuguese':
+		'Portugalski',
+	'Polish':
+		'Poljski',
+	'Russian':
+		'Ruski',
+	'Spanish':
+		'Španski',
+	'Italian':
+		'Italijanski',
+	'French':
+		'Francuski',
+	'German':
+		'Nemački',
+	
+	// FAQ
+	
+	'What are the system requirements for Unchecky?':
+		'Koji su sistemski zahtevi za Unchecky?',
+	'Unchecky supports Windows XP and above.':
+		'Unchecky podržava Windows XP i novije verzije.',
+	
+	'My antivirus flags Unchecky as malicious.':
+		'Moj antivirus je označio Unchecky kao zlonameran.',
+	'It\'s a false positive. Please report about it to your antivirus company. Note that Unchecky was tested thoroughly by Softpedia and <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#status">was found 100% clean</a>.':
+		'To je lažna uzbuna. Molimo Vas da obavestite kompaniju antivirusa. Imajte na umu da je Unchecky testiran od strane Softpedije i <a href="http://www.softpedia.com/get/System/OS-Enhancements/Unchecky.shtml#status">100% je siguran</a>.',
+	
+	'What is the best way to report a bug or suggest a feature?':
+		'Koji je najbolji način za prijavljivanje grešaka ili predlaganje funkcija?',
+	'Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a>.':
+		'Pošaljite svoje povratne informacije na <a href="http://unchecky.userecho.com/">UserEcho</a>.',
+	
+	'How do I uninstall Unchecky?':
+		'Kako mogu deinstalirati Unchecky?',
+	'Unchecky can be uninstalled from the Windows Control Panel, like any other program. See instructions for <a href="https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs">Windows 10</a> or <a href="https://support.microsoft.com/en-us/kb/2601726">Windows 7</a>.':
+		'Unchecky se može deinstalirati iz Windows kontrolne table, kao i svi drugi programi. Pogledajte uputstva za <a href="https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs">Windows 10</a> ili <a href="https://support.microsoft.com/en-us/kb/2601726">Windows 7</a>.',
+	'If the standard method fails, please try to use the <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, which is designed to uninstall corrupted Unchecky installations.':
+		'Ako standardni način ne radi, molimo  koristite <a href="files/unchecky_cleaner.exe">Unchecky Cleaner Tool</a>, koji je dizajniran za deinstalaciju oštećenih instalacija Uncheckyja.',
+	
+	'I want to translate Unchecky.':
+		'Želim prevesti Unchecky.',
+	'The translation files of the Unchecky program and website can be found on the <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a> GitHub repository.':
+		'Datoteke za prevođenje nalaze se na GitHub-u <a href="https://github.com/unchecky/unchecky-translate">unchecky-translate</a>.',
+	
+	'Is it possible to install Unchecky silently?':
+		'Da li je moguće izvršiti tihu instalaciju programa Unchecky?',
+	'Yes, run the installer with the <code>-install</code> command line switch. For a list of supported command line switches, run <code>unchecky_setup.exe -?</code>.':
+		'Da, pokrenite instaler sa <code>-install</code> komandom. Za listu podržanih komandi, pokrenite <code>unchecky_setup.exe -?</code>.',
+	
+	'Is there an MSI installer? I\'d like to install Unchecky remotely using Group Policy.':
+		'Da li nudite MSI installer? Želimda instaliram Unchecky udaljeno korišćenjem grupnih pravila (Group Policy).',
+	'There is an experimental MSI installer, it can be downloaded <a href="files/other/unchecky_setup.msi">here</a>. Please note that the installer wasn\'t thoroughly tested, so use it with caution.':
+		'Postoji eksperimentalan MSI instaler, možete ga preuzeti <a href="files/other/unchecky_setup.msi">ovde</a>. Molimo imajte na umu da instaler nije kompletno testiran, pa ga koristite s oprezom.',
+	
+	// Contact
+	
+	'Please don\'t use this contact form to report bugs or suggest features. Post your feedback on the <a href="http://unchecky.userecho.com/">UserEcho page</a> instead.':
+		'Molimo Vas da ne koristite ovaj obrazac za prijavu grešaka ili za predlog funkcija. Svoje povratne informacije možete slati na <a href="http://unchecky.userecho.com/">UserEcho</a>.',
+	
+	'First Name':
+		'Ime',
+	'Last Name':
+		'Prezime',
+	'Email Address':
+		'E-mail adresa',
+	'Subject':
+		'Predmet',
+	'Message':
+		'Poruka',
+	'Send':
+		'Pošalji',
+	
+	'Please fill in all fields.':
+		'Molimo ispunite sva polja.',
+	'Please enter a valid email address.':
+		'Molimo unesite ispravnu e-mail adresu.',
+	'Could not complete your request.':
+		'Nemoguće izvršiti zahtev.',
+	'The message has been sent successfully.':
+		'Vaša poruka je uspešno poslata.',
+	
+	// 404
+	
+	'Not found':
+		'Nije pronađeno',
+	
+	'Sorry but the page you are looking for cannot be found.':
+		'Isprike, ali stranica koju tražite nije pronađena.',
+	
+}
